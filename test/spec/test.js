4365ece1a7e0fd5a5ba8549a2e9ae48abb7380f9
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-02-25 22:51:36 -0800

    Better test

--- test/spec/test.js
+++ test/spec/test.js
@@ -8,12 +8,13 @@
     describe('wrap_elements', function () {
         describe('default youtube wrapping', function () {
             beforeEach(function() {
-                $('body').add('<iframe width="560" height="315" src="https://www.youtube.com/embed/VpXUIh7rlWI" frameborder="0" allowfullscreen></iframe>');
+                $('body').add('<iframe id="youtube_video" width="560" height="315" src="https://www.youtube.com/embed/VpXUIh7rlWI" frameborder="0" allowfullscreen></iframe>');
                 wrapElements('body');
             });
 
             it('should wrap the element', function () {
                 $('.deckard_extension').should.exist;
+                $('.deckard_extension').should.have.descendents('iframe#youtube_video');
             });
 
             afterEach(function() {
