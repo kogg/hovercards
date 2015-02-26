c9399fc537c815b345cade77ea3fbe137f42b620
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-02-25 22:42:57 -0800

    Unit Tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,12 +1,23 @@
-/* global describe, it */
+/* global describe, it, $, beforeEach, afterEach */
+/* global wrapElements */
+/*jshint expr:true */
 
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+    describe('wrap_elements', function () {
+        describe('default youtube wrapping', function () {
+            beforeEach(function() {
+                $('body').add('<iframe width="560" height="315" src="https://www.youtube.com/embed/VpXUIh7rlWI" frameborder="0" allowfullscreen></iframe>');
+                wrapElements();
+            });
 
+            it('should wrap the element', function () {
+                $('.deckard_extension').should.exist;
+            });
+
+            afterEach(function() {
+                $('body').empty();
             });
         });
     });
