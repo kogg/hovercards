4ef59f027eadba0a412f5fc0f466b38fe517550d
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-02-25 20:23:02 -0800

    Nyan reporter for git commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,7 +121,8 @@ module.exports = function (grunt) {
             all: {
                 options: {
                     run: true,
-                    urls: ['http://localhost:<%= connect.options.port %>/index.html']
+                    urls: ['http://localhost:<%= connect.options.port %>/index.html'],
+                    reporter: 'Nyan'
                 }
             }
         },
