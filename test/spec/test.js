d06419bd097862e4b6952b82ce35d51b81939e44
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-02-26 14:06:20 -0800

    Unit Tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,10 +3,9 @@
 'use strict';
 
 (function() {
-
     /* global wrapElements */
     describe('wrapElements', function() {
-        describe('default youtube embed', function() {
+        describe('default iframes', function() {
             it('should wrap youtube iframes', function() {
                 wrapElements('#sandbox');
                 $('#sandbox > iframe#youtube_video').should.not.exist;
@@ -37,8 +36,48 @@
             });
 
             beforeEach(function() {
-                $('#sandbox').append('<iframe id="youtube_video" width="560" height="315" src="https://www.youtube.com/embed/VpXUIh7rlWI" frameborder="0" allowfullscreen></iframe>');
+                $('#sandbox').append('<iframe id="youtube_video" src="https://www.youtube.com/embed/VpXUIh7rlWI"></iframe>');
                 $('#sandbox').append('<iframe id="not_youtube_video"></iframe>');
+            });
+
+            afterEach(function() {
+                $('#sandbox').empty();
+            });
+        });
+
+        describe('object tags', function() {
+            it('should wrap youtube object', function() {
+                wrapElements('#sandbox');
+                $('#sandbox > object#youtube_video').should.not.exist;
+                $('#sandbox > .deckard_extension > object#youtube_video').should.exist;
+            });
+
+            it('shouldn\'t wrap other objects', function() {
+                wrapElements('#sandbox');
+                $('#sandbox > object#not_youtube_video').should.exist;
+                $('#sandbox > .deckard_extension > object#not_youtube_video').should.not.exist;
+            });
+
+            it('should load minimal content under the object', function() {
+                // Mocking sending the message
+                var originalSendMessage = chrome.runtime.sendMessage;
+                chrome.runtime.sendMessage = function(message, callback) {
+                    message.cmd.should.equal('load_html');
+                    message.fileName.should.equal('minimal.html');
+                    callback('Minimal Content');
+                };
+
+                wrapElements('#sandbox');
+                $('#sandbox > .deckard_extension > object#youtube_video + .deckard_minimal').should.exist;
+                $('#sandbox > .deckard_extension > object#youtube_video + .deckard_minimal').should.have.html('Minimal Content');
+
+                // Undo the mocking
+                chrome.runtime.sendMessage = originalSendMessage;
+            });
+
+            beforeEach(function() {
+                $('#sandbox').append('<object id="youtube_video" data="https://www.youtube.com/v/VpXUIh7rlWI"></object>');
+                $('#sandbox').append('<object id="not_youtube_video"></object>');
             });
 
             afterEach(function() {
