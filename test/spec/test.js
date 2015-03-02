33f7d83d262d071163a126df108b7cdef2c9e66f
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-01 21:45:43 -0800

    This fixes some stuff

--- test/spec/test.js
+++ test/spec/test.js
@@ -240,11 +240,9 @@
 
     // This guards against a crazy phantomjs error we're getting where all the tests run, but the console shows none of it.
     // It used to only happen on fail, but now its happening all the time.
-    /*
     it('takes time', function(done) {
         setTimeout(function() {
             done();
         }, 1);
     });
-    */
 })();
