8e17ffcad19ee0204c9f025431cbf961a7fdff46
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-02 23:25:23 -0800

    Button position absolute

--- test/spec/button.js
+++ test/spec/button.js
@@ -6,10 +6,15 @@
     /* global button */
     describe('button', function() {
         it('should have class deckard-button', function() {
-            button().should.have.class('deckard-button');
+            $('#sandbox').append(button());
+            $('#sandbox > .deckard-button').should.exist;
         });
 
-        it('should have position absolute');
+        it('should have position absolute', function() {
+            $('#sandbox').append(button());
+            $('#sandbox > .deckard-button').should.have.css('position', 'absolute');
+        });
+
         it('should load it\'s content');
 
         afterEach(function() {
