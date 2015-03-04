1d144d28fdee7568b121efbf6e24a900a30bcbcf
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-04 01:55:58 -0800

    sidebar tests should clean up after themselves

--- test/spec/sidebar-put-in-area.js
+++ test/spec/sidebar-put-in-area.js
@@ -8,5 +8,17 @@
                 sidebar.putInArea('#sandbox').should.have.class('deckard-sidebar');
             });
         });
+
+        var sandbox;
+
+        before(function() {
+            sandbox = sinon.sandbox.create();
+        });
+
+        afterEach(function() {
+            $('#sandbox').empty();
+            $('#sandbox').off();
+            sandbox.restore();
+        });
     });
 })();
