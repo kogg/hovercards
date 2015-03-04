8c4581b29e5b366f5e31e15225f838f5b04a1673
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-03 23:57:11 -0800

    Isolating chromereload logic

--- app/scripts/chromereload.js
+++ app/scripts/chromereload.js
@@ -4,19 +4,21 @@
 // The reload client has a compatibility with livereload.
 // WARNING: only supports reload command.
 
-var LIVERELOAD_HOST = 'localhost:';
-var LIVERELOAD_PORT = 35729;
-var connection = new WebSocket('ws://' + LIVERELOAD_HOST + LIVERELOAD_PORT + '/livereload');
+(function() {
+    var LIVERELOAD_HOST = 'localhost:';
+    var LIVERELOAD_PORT = 35729;
+    var connection = new WebSocket('ws://' + LIVERELOAD_HOST + LIVERELOAD_PORT + '/livereload');
 
-connection.onerror = function (error) {
-    console.log('reload connection got error:', error);
-};
+    connection.onerror = function (error) {
+        console.log('reload connection got error:', error);
+    };
 
-connection.onmessage = function (e) {
-    if (e.data) {
-        var data = JSON.parse(e.data);
-        if (data && data.command === 'reload') {
-            chrome.runtime.reload();
+    connection.onmessage = function (e) {
+        if (e.data) {
+            var data = JSON.parse(e.data);
+            if (data && data.command === 'reload') {
+                chrome.runtime.reload();
+            }
         }
-    }
-};
+    };
+}());
