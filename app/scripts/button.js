a32e3ef683134ec95c4d7b9e7ddfe93c611af791
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-03 22:57:33 -0800

    Hover timeouts

--- app/scripts/button.js
+++ app/scripts/button.js
@@ -5,11 +5,25 @@ function button(video) {
     var buttonObj = $('<div class="deckard-button"></div>');
     video = $(video);
 
+    var timeout;
+
     buttonObj.offset(video.offset());
-    $(buttonObj).add(video).hover(function() {
+    $(buttonObj).hover(function() {
         buttonObj.css('opacity', 1);
+        clearTimeout(timeout);
     }, function() {
         buttonObj.css('opacity', 0);
+        clearTimeout(timeout);
+    });
+
+    video.hover(function() {
+        buttonObj.css('opacity', 1);
+        timeout = setTimeout(function() {
+            buttonObj.css('opacity', 0);
+        }, 1000);
+    }, function() {
+        buttonObj.css('opacity', 0);
+        clearTimeout(timeout);
     });
 
     chrome.runtime.sendMessage({ cmd: 'load_html', fileName: 'button.html' }, function(html) {
--- test/spec/button.js
+++ test/spec/button.js
@@ -4,24 +4,24 @@
     /* global button */
     describe('button', function() {
         it('should have class deckard-button', function() {
-            button('#sandbox').appendTo('#sandbox')
+            button('#video').appendTo('#sandbox')
                 .should.have.class('deckard-button');
         });
 
         it('should be transparent', function() {
-            button('#sandbox').appendTo('#sandbox')
+            button('#video').appendTo('#sandbox')
                 .should.have.css('opacity', '0');
         });
 
         it('should have position absolute', function() {
-            button('#sandbox').appendTo('#sandbox')
+            button('#video').appendTo('#sandbox')
                 .should.have.css('position', 'absolute');
         });
 
         it('should load it\'s content', function() {
             sandbox.stub(chrome.runtime, 'sendMessage')
                 .yields('Button Content');
-            var buttonObj = button('#sandbox').appendTo('#sandbox');
+            var buttonObj = button('#video').appendTo('#sandbox');
 
             chrome.runtime.sendMessage
                 .should.have.been.calledWith({ cmd: 'load_html', fileName: 'button.html' });
@@ -29,42 +29,75 @@
                 .should.have.html('Button Content');
         });
 
-        it('should be opaque on mouseenter', function() {
-            button('#sandbox').appendTo('#sandbox')
-                .mouseenter()
-                .should.have.css('opacity', '1');
+        describe('hovering', function() {
+            it('should be opaque on mouseenter', function() {
+                button('#video').appendTo('#sandbox')
+                    .mouseenter()
+                    .should.have.css('opacity', '1');
+            });
+
+            it('should be transparent on mouseleave', function() {
+                button('#video').appendTo('#sandbox')
+                    .mouseenter()
+                    .mouseleave()
+                    .should.have.css('opacity', '0');
+            });
+
+            it('should still be opaque 1 second after mouseenter', function(done) {
+                var buttonObj = button('#video').appendTo('#sandbox');
+
+                buttonObj
+                    .mouseenter();
+                setTimeout(function() {
+                    buttonObj
+                        .should.have.css('opacity', '1');
+                    done();
+                }, 1000);
+            });
         });
 
-        it('should be transparent on mouseleave', function() {
-            button('#sandbox').appendTo('#sandbox')
-                .mouseenter()
-                .mouseleave()
-                .should.have.css('opacity', '0');
-        });
+        describe('video hovering', function() {
+            it('should be opaque on video mouseenter', function() {
+                console.log('problem');
+                var buttonObj = button('#video').appendTo('#sandbox');
 
-        it('should be opaque on element mouseenter', function() {
-            var buttonObj = button('#sandbox').appendTo('#sandbox');
+                $('#video')
+                    .mouseenter();
+                buttonObj
+                    .should.have.css('opacity', '1');
+            });
 
-            $('#sandbox')
-                .mouseenter();
-            buttonObj
-                .should.have.css('opacity', '1');
-        });
+            it('should be transparent on video mouseleave', function() {
+                var buttonObj = button('#video').appendTo('#sandbox');
 
-        it('should be transparent on element mouseleave', function() {
-            var buttonObj = button('#sandbox').appendTo('#sandbox');
+                $('#video')
+                    .mouseenter()
+                    .mouseleave();
+                buttonObj
+                    .should.have.css('opacity', '0');
+            });
 
-            $('#sandbox')
-                .mouseenter()
-                .mouseleave();
-            buttonObj
-                .should.have.css('opacity', '0');
+            it('should be transparent 1 second after video mouseenter', function(done) {
+                var buttonObj = button('#video').appendTo('#sandbox');
+
+                $('#video')
+                    .mouseenter();
+                setTimeout(function() {
+                    buttonObj
+                        .should.have.css('opacity', '0');
+                    done();
+                }, 1000);
+            });
         });
 
         var sandbox;
 
         before(function() {
             sandbox = sinon.sandbox.create();
+        });
+
+        beforeEach(function() {
+            $('#sandbox').append('<div id="video"></div>');
         });
 
         afterEach(function() {
