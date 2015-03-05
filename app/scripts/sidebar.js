fc7c70e483bd64a6095eec9e0b6a375d7ceaab74
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-05 13:58:46 -0800

    Sidebar sticks around if you don't show disinterest soon enough

--- app/scripts/sidebar.js
+++ app/scripts/sidebar.js
@@ -4,14 +4,21 @@ define('sidebar', ['jquery'], function($) {
     return function sidebar() {
         var obj = $('<div class="deckard-sidebar"></div>').append($('<iframe frameborder="0"></iframe>').prop('src', chrome.extension.getURL('sidebar.html')));
 
+        var stayOpen = false;
+        var stayOpenTimeout;
+
         chrome.runtime.onMessage.addListener(function(request, sender) {
             /*jshint unused:false */
-            if (request.msg === 'sidebar') {
-                if (request.key === 'display') {
-                    if (request.value === 'visible') {
-                        obj.show();
-                    } else if (request.value === 'uninterested') {
+            if (request.msg === 'sidebar' && request.key === 'display') {
+                if (request.value === 'visible') {
+                    obj.show();
+                    stayOpenTimeout = setTimeout(function() {
+                        stayOpen = true;
+                    }, 2000);
+                } else if (request.value === 'uninterested') {
+                    if (!stayOpen) {
                         obj.hide();
+                        clearTimeout(stayOpenTimeout);
                     }
                 }
             }
--- test/spec/sidebar.js
+++ test/spec/sidebar.js
@@ -30,16 +30,33 @@ define(['sidebar', 'sinon'], function(sidebar, sinon) {
                 sidebarObj.should.be.visible;
             });
 
-            it('should be hidden when uninterested', function() {
+            it('should be hidden when uninterested within 2 seconds of showing', function() {
+                var clock = sandbox.useFakeTimers();
                 sandbox.stub(chrome.runtime.onMessage, 'addListener');
                 var sidebarObj = sidebar().appendTo('#sandbox');
                 chrome.runtime.onMessage.addListener.yield({ msg: 'sidebar', key: 'display', value: 'visible' },
                                                            {},
                                                            $.noop);
+                clock.tick(1999);
+                sidebarObj.should.be.visible;
                 chrome.runtime.onMessage.addListener.yield({ msg: 'sidebar', key: 'display', value: 'uninterested' },
                                                            {},
                                                            $.noop);
                 sidebarObj.should.be.hidden;
+            });
+
+            it('should stay visible when uninterested over 2 seconds of showing', function() {
+                var clock = sandbox.useFakeTimers();
+                sandbox.stub(chrome.runtime.onMessage, 'addListener');
+                var sidebarObj = sidebar().appendTo('#sandbox');
+                chrome.runtime.onMessage.addListener.yield({ msg: 'sidebar', key: 'display', value: 'visible' },
+                                                           {},
+                                                           $.noop);
+                clock.tick(2000);
+                chrome.runtime.onMessage.addListener.yield({ msg: 'sidebar', key: 'display', value: 'uninterested' },
+                                                           {},
+                                                           $.noop);
+                sidebarObj.should.be.visible;
             });
         });
 
