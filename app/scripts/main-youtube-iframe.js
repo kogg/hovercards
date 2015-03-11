d351f9c8e7d4c812dc79e66d47f962da664445cd
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-10 20:06:53 -0700

    Include youtube

--- app/scripts/main-youtube-iframe.js
+++ app/scripts/main-youtube-iframe.js
@@ -1,6 +1,8 @@
 'use strict';
 
-require(['jquery', 'youtube-button'], function($, youtubeButton) {
-    /* globals purl:true */
-    $('body').prepend(youtubeButton('body #player', purl(document.URL).segment(-1)));
+require(['youtube'], function() {
+    require(['jquery', 'youtube-button'], function($, youtubeButton) {
+        /* globals purl:true */
+        $('body').prepend(youtubeButton('body #player', purl(document.URL).segment(-1)));
+    });
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,25 +3,27 @@
 require(['jquery', 'sidebar'], function($, sidebar) {
     sidebar().appendTo('body');
 });
-require(['jquery', 'youtube-button'], function($, youtubeButton) {
-    $('body')
-        .find('object[data*="youtube.com/v/"], embed[src*="youtube.com/v/"]')
-        .each(function() {
-            /* globals purl:true */
-            var video = $(this);
-            youtubeButton(video, purl(video.prop('data') || video.prop('src')).segment(-1)).insertBefore(video);
-        });
-});
-require(['jquery', 'trigger'], function($, trigger) {
-    var youtubeLinkSelector = 'a[href*="youtube.com/watch"]';
-    if (purl(document.URL).attr('host') === 'www.youtube.com') {
-        youtubeLinkSelector += ',a[href^="/watch"]';
-    }
-    $('body')
-        .find(youtubeLinkSelector)
-        .each(function() {
-            /* globals purl:true */
-            var link = $(this);
-            trigger(link, 'youtube', purl(link.attr('href')).param('v'));
-        });
+require(['youtube'], function() {
+    require(['jquery', 'youtube-button'], function($, youtubeButton) {
+        $('body')
+            .find('object[data*="youtube.com/v/"], embed[src*="youtube.com/v/"]')
+            .each(function() {
+                /* globals purl:true */
+                var video = $(this);
+                youtubeButton(video, purl(video.prop('data') || video.prop('src')).segment(-1)).insertBefore(video);
+            });
+    });
+    require(['jquery', 'trigger'], function($, trigger) {
+        var youtubeLinkSelector = 'a[href*="youtube.com/watch"]';
+        if (purl(document.URL).attr('host') === 'www.youtube.com') {
+            youtubeLinkSelector += ',a[href^="/watch"]';
+        }
+        $('body')
+            .find(youtubeLinkSelector)
+            .each(function() {
+                /* globals purl:true */
+                var link = $(this);
+                trigger(link, 'youtube', purl(link.attr('href')).param('v'));
+            });
+    });
 });
