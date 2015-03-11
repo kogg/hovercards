d9781f1b423eaf2011a11ca96d2dacb06d65237a
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-11 10:29:45 -0700

    Changed deckard to HoverToast in multiple places

--- Gruntfile.js
+++ Gruntfile.js
@@ -296,7 +296,7 @@ module.exports = function (grunt) {
                 options: {
                     archive: function() {
                         var manifest = grunt.file.readJSON('app/manifest.json');
-                        return 'package/deckard-' + manifest.version + '.zip';
+                        return 'package/HoverToast-' + manifest.version + '.zip';
                     }
                 },
                 files: [{
