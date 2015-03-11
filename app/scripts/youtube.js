2faf47eb54a1cc92c5c4b90f2235ab2ff03b266a
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-10 22:11:52 -0700

    Breaking apart inject

--- app/scripts/youtube.js
+++ app/scripts/youtube.js
@@ -1,6 +1,41 @@
 'use strict';
 
 define('youtube', ['jquery'], function($) {
+    function injectButtonOnPlayer(body) {
+        require(['youtube-button'], function(youtubeButton) {
+            /* globals purl:true */
+            youtubeButton(body.children('#player'), purl(document.URL).segment(-1)).prependTo(body);
+        });
+    }
+
+    function injectButtonsOnObjectsAndEmbeds(body) {
+        require(['youtube-button'], function(youtubeButton) {
+            body
+                .find('object[data*="youtube.com/v/"], embed[src*="youtube.com/v/"]')
+                .each(function() {
+                    /* globals purl:true */
+                    var video = $(this);
+                    youtubeButton(video, purl(video.prop('data') || video.prop('src')).segment(-1)).insertBefore(video);
+                });
+        });
+    }
+
+    function injectTriggersOnLinks(body) {
+        require(['trigger'], function(trigger) {
+            var youtubeLinkSelector = 'a[href*="youtube.com/watch"]';
+            if (purl(document.URL).attr('host') === 'www.youtube.com') {
+                youtubeLinkSelector += ',a[href^="/watch"]';
+            }
+            body
+                .find(youtubeLinkSelector)
+                .each(function() {
+                    /* globals purl:true */
+                    var link = $(this);
+                    trigger(link, 'youtube', purl(link.attr('href')).param('v'));
+                });
+        });
+    }
+
     function inject(body, context) {
         if (!body) {
             body = 'body';
@@ -8,34 +43,11 @@ define('youtube', ['jquery'], function($) {
         body = $(body);
         switch (context) {
             case 'youtube iframe':
-                require(['youtube-button'], function(youtubeButton) {
-                    /* globals purl:true */
-                    youtubeButton(body.children('#player'), purl(document.URL).segment(-1)).prependTo(body);
-                });
+                injectButtonOnPlayer(body);
                 break;
             default:
-                require(['youtube-button'], function(youtubeButton) {
-                    body
-                        .find('object[data*="youtube.com/v/"], embed[src*="youtube.com/v/"]')
-                        .each(function() {
-                            /* globals purl:true */
-                            var video = $(this);
-                            youtubeButton(video, purl(video.prop('data') || video.prop('src')).segment(-1)).insertBefore(video);
-                        });
-                });
-                require(['trigger'], function(trigger) {
-                    var youtubeLinkSelector = 'a[href*="youtube.com/watch"]';
-                    if (purl(document.URL).attr('host') === 'www.youtube.com') {
-                        youtubeLinkSelector += ',a[href^="/watch"]';
-                    }
-                    body
-                        .find(youtubeLinkSelector)
-                        .each(function() {
-                            /* globals purl:true */
-                            var link = $(this);
-                            trigger(link, 'youtube', purl(link.attr('href')).param('v'));
-                        });
-                });
+                injectButtonsOnObjectsAndEmbeds(body);
+                injectTriggersOnLinks(body);
                 break;
         }
     }
