611d49195f27aa0b1875c8f3c58c69f01f6be52c
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-10 20:36:43 -0700

    Moved injection logic into youtube.js

--- app/scripts/main-youtube-iframe.js
+++ app/scripts/main-youtube-iframe.js
@@ -2,8 +2,4 @@
 
 require(['youtube'], function(youtube) {
     youtube.inject('body', 'youtube iframe');
-    require(['jquery', 'youtube-button'], function($, youtubeButton) {
-        /* globals purl:true */
-        $('body').prepend(youtubeButton('body #player', purl(document.URL).segment(-1)));
-    });
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,26 +5,4 @@ require(['jquery', 'sidebar'], function($, sidebar) {
 });
 require(['youtube'], function(youtube) {
     youtube.inject('body');
-    require(['jquery', 'youtube-button'], function($, youtubeButton) {
-        $('body')
-            .find('object[data*="youtube.com/v/"], embed[src*="youtube.com/v/"]')
-            .each(function() {
-                /* globals purl:true */
-                var video = $(this);
-                youtubeButton(video, purl(video.prop('data') || video.prop('src')).segment(-1)).insertBefore(video);
-            });
-    });
-    require(['jquery', 'trigger'], function($, trigger) {
-        var youtubeLinkSelector = 'a[href*="youtube.com/watch"]';
-        if (purl(document.URL).attr('host') === 'www.youtube.com') {
-            youtubeLinkSelector += ',a[href^="/watch"]';
-        }
-        $('body')
-            .find(youtubeLinkSelector)
-            .each(function() {
-                /* globals purl:true */
-                var link = $(this);
-                trigger(link, 'youtube', purl(link.attr('href')).param('v'));
-            });
-    });
 });
