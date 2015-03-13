aa1d949c5d5e3965b5e650c872a99ba744e5a4cc
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-13 15:20:49 -0700

    Restructuring tests a bit

--- test/spec/card-handler.js
+++ test/spec/card-handler.js
@@ -11,63 +11,65 @@ describe('card-handler', function() {
         });
     });
 
-    it('should be visible when called', function() {
-        var cardObj = $('<div id="something-card" style="display: none;"></div>').appendTo('#sandbox');
+    describe('handled cards', function() {
+        it('should be visible', function() {
+            var cardObj = $('<div id="something-card" style="display: none;"></div>').appendTo('#sandbox');
 
-        var handleCard = cardHandler('#sandbox');
-        handleCard('something');
+            var handleCard = cardHandler('#sandbox');
+            handleCard('something');
 
-        cardObj.should.be.visible;
-    });
+            cardObj.should.be.visible;
+        });
 
-    it('should reorder the cards in the order they\'re called', function() {
-        $('<div id="third-card"></div>').appendTo('#sandbox');
-        $('<div id="second-card"></div>').appendTo('#sandbox');
-        $('<div id="first-card"></div>').appendTo('#sandbox');
+        it('should be reordered', function() {
+            $('<div id="third-card"></div>').appendTo('#sandbox');
+            $('<div id="second-card"></div>').appendTo('#sandbox');
+            $('<div id="first-card"></div>').appendTo('#sandbox');
 
-        var handleCard = cardHandler('#sandbox');
-        handleCard('first');
-        handleCard('second');
-        handleCard('third');
+            var handleCard = cardHandler('#sandbox');
+            handleCard('first');
+            handleCard('second');
+            handleCard('third');
 
-        $('#sandbox :eq(0)').should.have.id('first-card');
-        $('#sandbox :eq(1)').should.have.id('second-card');
-        $('#sandbox :eq(2)').should.have.id('third-card');
-    });
+            $('#sandbox :eq(0)').should.have.id('first-card');
+            $('#sandbox :eq(1)').should.have.id('second-card');
+            $('#sandbox :eq(2)').should.have.id('third-card');
+        });
 
-    it('should be called on more cards', function() {
-        $('<div id="first-card" style="display: none;" data-more=\'["third"]\'></div>').appendTo('#sandbox');
-        $('<div id="second-card" style="display: none;"></div>').appendTo('#sandbox');
-        var third = $('<div id="third-card" style="display: none;"></div>').appendTo('#sandbox');
+        it('should handle more cards', function() {
+            $('<div id="first-card" style="display: none;" data-more=\'["third"]\'></div>').appendTo('#sandbox');
+            $('<div id="second-card" style="display: none;"></div>').appendTo('#sandbox');
+            var third = $('<div id="third-card" style="display: none;"></div>').appendTo('#sandbox');
 
-        var handleCard = cardHandler('#sandbox');
-        handleCard('first');
+            var handleCard = cardHandler('#sandbox');
+            handleCard('first');
 
-        third.should.be.visible;
-    });
+            third.should.be.visible;
+        });
 
-    it('should be called on five cards max', function() {
-        var first = $('<div id="first-card" style="display: none;"></div>').appendTo('#sandbox');
-        var second = $('<div id="second-card" style="display: none;"></div>').appendTo('#sandbox');
-        var third = $('<div id="third-card" style="display: none;"></div>').appendTo('#sandbox');
-        var fourth = $('<div id="fourth-card" style="display: none;"></div>').appendTo('#sandbox');
-        var fifth = $('<div id="fifth-card" style="display: none;"></div>').appendTo('#sandbox');
-        var sixth = $('<div id="sixth-card" style="display: none;"></div>').appendTo('#sandbox');
+        it('should be capped at five cards', function() {
+            var first = $('<div id="first-card" style="display: none;"></div>').appendTo('#sandbox');
+            var second = $('<div id="second-card" style="display: none;"></div>').appendTo('#sandbox');
+            var third = $('<div id="third-card" style="display: none;"></div>').appendTo('#sandbox');
+            var fourth = $('<div id="fourth-card" style="display: none;"></div>').appendTo('#sandbox');
+            var fifth = $('<div id="fifth-card" style="display: none;"></div>').appendTo('#sandbox');
+            var sixth = $('<div id="sixth-card" style="display: none;"></div>').appendTo('#sandbox');
 
-        var handleCard = cardHandler('#sandbox');
-        handleCard('first');
-        handleCard('second');
-        handleCard('third');
-        handleCard('fourth');
-        handleCard('fifth');
-        handleCard('sixth');
+            var handleCard = cardHandler('#sandbox');
+            handleCard('first');
+            handleCard('second');
+            handleCard('third');
+            handleCard('fourth');
+            handleCard('fifth');
+            handleCard('sixth');
 
-        first.should.be.visible;
-        second.should.be.visible;
-        third.should.be.visible;
-        fourth.should.be.visible;
-        fifth.should.be.visible;
-        sixth.should.not.be.visible;
+            first.should.be.visible;
+            second.should.be.visible;
+            third.should.be.visible;
+            fourth.should.be.visible;
+            fifth.should.be.visible;
+            sixth.should.not.be.visible;
+        });
     });
 
     afterEach(function() {
