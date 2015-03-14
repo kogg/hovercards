205d7b2e6fe964a9438931d3d23c2a252f4dc804
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-13 22:20:04 -0700

    Only five cards total

--- app/scripts/cards-controller.js
+++ app/scripts/cards-controller.js
@@ -3,6 +3,9 @@
 define(['angular-app'], function(app) {
     app.controller('CardsController', ['$scope', function($scope) {
         function addCard(card) {
+            if ($scope.cards.length >= 5) {
+                return;
+            }
             for (var i = 0; i < $scope.cards.length; i++) {
                 if ($scope.cards[i].content === card.content && $scope.cards[i].id === card.id) {
                     return;
--- test/spec/cards-controller.js
+++ test/spec/cards-controller.js
@@ -21,10 +21,24 @@ describe('cards-controller', function() {
             $scope.cards.should.deep.equal([{ content: 'something', id: 'ID' }]);
         });
 
-        it('shouldn\'t push the same card twice', function() {
+        it('should not push the same card twice', function() {
             $scope.addCard({ content: 'something', id: 'ID' });
             $scope.addCard({ content: 'something', id: 'ID' });
             $scope.cards.should.deep.equal([{ content: 'something', id: 'ID' }]);
+        });
+
+        it('should be called a maximum of five times', function() {
+            $scope.addCard({ content: 'something1', id: 'ID1' });
+            $scope.addCard({ content: 'something2', id: 'ID2' });
+            $scope.addCard({ content: 'something3', id: 'ID3' });
+            $scope.addCard({ content: 'something4', id: 'ID4' });
+            $scope.addCard({ content: 'something5', id: 'ID5' });
+            $scope.addCard({ content: 'something6', id: 'ID6' });
+            $scope.cards.should.deep.equal([{ content: 'something1', id: 'ID1' },
+                                            { content: 'something2', id: 'ID2' },
+                                            { content: 'something3', id: 'ID3' },
+                                            { content: 'something4', id: 'ID4' },
+                                            { content: 'something5', id: 'ID5' }]);
         });
     });
 });
