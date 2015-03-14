e127f41a41092b1187fd3f080b9f77b9f7546193
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-13 20:26:51 -0700

    Blowing away card-handler

--- app/scripts/main-sidebar.js
+++ app/scripts/main-sidebar.js
@@ -1,6 +1,4 @@
 'use strict';
 
-require(['purl', 'card-handler'], function(purl, cardHandler) {
-    var handler = cardHandler('body');
-    handler.handleCard(purl(document.URL).param('content'));
+require([], function() {
 });
