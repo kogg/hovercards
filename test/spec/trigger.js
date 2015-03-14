fc7d973c493f18927ec9617f219df2b24a41e3ee
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-13 17:10:42 -0700

    Simplifying trigger tests

--- test/spec/trigger.js
+++ test/spec/trigger.js
@@ -2,54 +2,14 @@
 
 describe('trigger', function() {
     var sandbox = sinon.sandbox.create();
-    var trigger;
+    var triggerObj;
 
     beforeEach(function(done) {
         $('#sandbox').append('<div id="trigger"></div>');
-        require(['trigger'], function(_trigger) {
-            trigger = _trigger;
+        require(['trigger'], function(trigger) {
+            sandbox.stub(chrome.runtime, 'sendMessage');
+            triggerObj = trigger('#trigger', 'something', 'SOME_ID').appendTo('#sandbox');
             done();
-        });
-    });
-
-    it('should have data hovertoast_content', function() {
-        var obj = trigger('#trigger', 'something', 'SOME_ID').appendTo('#sandbox');
-        obj.should.have.data('hovertoast_content', 'something');
-    });
-
-    it('should have data hovertoast_id', function() {
-        var obj = trigger('#trigger', 'something', 'SOME_ID').appendTo('#sandbox');
-        obj.should.have.data('hovertoast_id', 'SOME_ID');
-    });
-
-    describe('when mouseenter', function() {
-        it('should send pre-load message', function() {
-            sandbox.stub(chrome.runtime, 'sendMessage');
-            var obj = trigger('#trigger', 'something', 'SOME_ID').appendTo('#sandbox');
-            chrome.runtime.sendMessage.should.not.have.been.calledWith({ msg: 'pre-load', content: 'something', id: 'SOME_ID' });
-            obj.mouseenter();
-            chrome.runtime.sendMessage.should.have.been.calledWith({ msg: 'pre-load', content: 'something', id: 'SOME_ID' });
-        });
-    });
-
-    describe('when mouseleave', function() {
-        it('should send uninterested message', function() {
-            sandbox.stub(chrome.runtime, 'sendMessage');
-            var obj = trigger('#trigger', 'something', 'SOME_ID').appendTo('#sandbox');
-            obj.mouseenter();
-            chrome.runtime.sendMessage.should.not.have.been.calledWith({ msg: 'interest', interested: null });
-            obj.mouseleave();
-            chrome.runtime.sendMessage.should.have.been.calledWith({ msg: 'interest', interested: null });
-        });
-    });
-
-    describe('when click', function() {
-        it('should send interested message', function() {
-            sandbox.stub(chrome.runtime, 'sendMessage');
-            var obj = trigger('#trigger', 'something', 'SOME_ID').appendTo('#sandbox');
-            chrome.runtime.sendMessage.should.not.have.been.calledWith({ msg: 'interest', interested: true });
-            obj.click();
-            chrome.runtime.sendMessage.should.have.been.calledWith({ msg: 'interest', interested: true });
         });
     });
 
@@ -57,5 +17,36 @@ describe('trigger', function() {
         $('#sandbox').empty();
         $('#sandbox').off();
         sandbox.restore();
+    });
+
+    it('should have data hovertoast_content', function() {
+        triggerObj.should.have.data('hovertoast_content', 'something');
+    });
+
+    it('should have data hovertoast_id', function() {
+        triggerObj.should.have.data('hovertoast_id', 'SOME_ID');
+    });
+
+    describe('when mouseenter', function() {
+        it('should send pre-load message', function() {
+            triggerObj.mouseenter();
+            chrome.runtime.sendMessage.should.have.been.calledWith({ msg: 'pre-load', content: 'something', id: 'SOME_ID' });
+        });
+    });
+
+    describe('when mouseleave', function() {
+        it('should send uninterested message', function() {
+            triggerObj.mouseenter();
+            chrome.runtime.sendMessage.should.not.have.been.calledWith({ msg: 'interest', interested: null });
+            triggerObj.mouseleave();
+            chrome.runtime.sendMessage.should.have.been.calledWith({ msg: 'interest', interested: null });
+        });
+    });
+
+    describe('when click', function() {
+        it('should send interested message', function() {
+            triggerObj.click();
+            chrome.runtime.sendMessage.should.have.been.calledWith({ msg: 'interest', interested: true });
+        });
     });
 });
