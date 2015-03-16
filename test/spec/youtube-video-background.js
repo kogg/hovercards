99999835dc2e7a421902cd8138d5ad473b5487d5
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-16 16:04:38 -0700

    should send youtube-video card

--- test/spec/youtube-video-background.js
+++ test/spec/youtube-video-background.js
@@ -22,5 +22,11 @@ describe('youtube-video-background', function() {
             chrome.tabs.sendMessage.should.have.been.calledWith('TAB_ID', sinon.match.has('msg', 'cards'));
             chrome.tabs.sendMessage.should.have.been.calledWith('TAB_ID', sinon.match.has('cards', sinon.match.array));
         });
+
+        it('should send youtube-video card', function() {
+            chrome.runtime.onMessage.addListener.yield({ msg: 'triggered', content: 'youtube-video', id: 'SOME_ID' }, { tab: { id: 'TAB_ID' } });
+            var cards = chrome.tabs.sendMessage.getCall(0).args[1].cards;
+            cards[0].should.have.property('content', 'youtube-video');
+        });
     });
 });
