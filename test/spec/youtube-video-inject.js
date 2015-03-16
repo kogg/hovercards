a171305eac1305e333fef3959a918d0aba55d082
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-16 15:22:05 -0700

    testing youtube-video-inject on embeds/objects

--- test/spec/youtube-video-inject.js
+++ test/spec/youtube-video-inject.js
@@ -35,9 +35,42 @@ describe('youtube-video-inject', function() {
             youtubeVideoInject(body, '#player', 'https://youtube.com/embed/QH2-TGUlwu4');
         });
 
-        it('attaches youtube-video-button to #player', function() {
-            youtubeVideoButton.should.have.been.calledWith(sinon.match(body.children('#player')));
-            youtubeVideoButton.should.have.been.calledWith(sinon.match.any, 'QH2-TGUlwu4');
+        it('should attach youtube-video-button to #player', function() {
+            youtubeVideoButton.should.have.been.calledWith(sinon.match(function(value) {
+                return value[0] === body.children('#player')[0];
+            }, 'wasn\'t matched with #player'));
+            youtubeVideoButton.should.always.have.been.calledWith(sinon.match.any, 'QH2-TGUlwu4');
+        });
+    });
+
+    describe('on "embeds/objects"', function() {
+        var body;
+        beforeEach(function() {
+            body = $('<div id="sandbox"><div id="player"></div></div>');
+            $('<embed id="embed" src="https://www.youtube.com/v/QH2-TGUlwu4">').appendTo(body);
+            $('<embed id="embed_bad">').appendTo(body);
+            $('<object id="object" data="https://www.youtube.com/v/QH2-TGUlwu4"></object>').appendTo(body);
+            $('<object id="object_bad"></object>').appendTo(body);
+            youtubeVideoInject(body, 'objects');
+        });
+
+        it('should attach youtube-video-button to youtube embeds/objects', function() {
+            youtubeVideoButton.should.have.been.calledWith(sinon.match(function(value) {
+                return value[0] === body.children('#embed')[0];
+            }, 'wasn\'t matched with #embed'));
+            youtubeVideoButton.should.have.been.calledWith(sinon.match(function(value) {
+                return value[0] === body.children('#object')[0];
+            }, 'wasn\'t matched with #object'));
+            youtubeVideoButton.should.always.have.been.calledWith(sinon.match.any, 'QH2-TGUlwu4');
+        });
+
+        it('should not attach youtube-video-button to other embeds/objects', function() {
+            youtubeVideoButton.should.not.have.been.calledWith(sinon.match(function(value) {
+                return value[0] === body.children('#embed_bad')[0];
+            }, 'wasn\'t matched with #embed_bad'));
+            youtubeVideoButton.should.not.have.been.calledWith(sinon.match(function(value) {
+                return value[0] === body.children('#object_bad')[0];
+            }, 'wasn\'t matched with #object_bad'));
         });
     });
 
