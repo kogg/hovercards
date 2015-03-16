5ffedd39eb7eb62bf802e9b3fc4c0e8a51beedc1
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-16 01:30:38 -0700

    youtube-video-background should respond to a triggered message with a cards message

---
+++ app/scripts/youtube-video-background.js
@@ -0,0 +1,12 @@
+'use strict';
+
+define([], function() {
+    return function youtubeVideoBackground() {
+        chrome.runtime.onMessage.addListener(function(request, sender) {
+            if (request.msg !== 'triggered' || request.content !== 'youtube-video') {
+                return;
+            }
+            chrome.tabs.sendMessage(sender.tab.id, { msg: 'cards' });
+        });
+    };
+});
---
+++ test/spec/youtube-video-background.js
@@ -0,0 +1,28 @@
+'use strict';
+
+describe('youtube-video-background', function() {
+    var sandbox = sinon.sandbox.create();
+
+    beforeEach(function(done) {
+        require(['youtube-video-background'], function(youtubeVideoBackground) {
+            sandbox.stub(chrome.tabs, 'sendMessage');
+            sandbox.stub(chrome.runtime.onMessage, 'addListener');
+            youtubeVideoBackground();
+            done();
+        });
+    });
+
+    afterEach(function() {
+        sandbox.restore();
+    });
+
+    describe('when receiving triggered message', function() {
+        beforeEach(function() {
+            chrome.runtime.onMessage.addListener.yield({ msg: 'triggered', content: 'youtube-video', id: 'SOME_ID' }, { tab: { id: 'TAB_ID' } }, $.noop);
+        });
+
+        it('should send cards message', function() {
+            chrome.tabs.sendMessage.should.have.been.calledWith('TAB_ID', { msg: 'cards' });
+        });
+    });
+});
