67963eed2b1ce7dc12133546e1658c150173350b
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-16 02:30:41 -0700

    if youtube-video-background calls the callback instead if given one

--- app/scripts/youtube-video-background.js
+++ app/scripts/youtube-video-background.js
@@ -2,11 +2,17 @@
 
 define([], function() {
     return function youtubeVideoBackground() {
-        chrome.runtime.onMessage.addListener(function(request, sender) {
+        chrome.runtime.onMessage.addListener(function(request, sender, callback) {
             if (request.msg !== 'triggered' || request.content !== 'youtube-video') {
                 return;
             }
-            chrome.tabs.sendMessage(sender.tab.id, { msg: 'cards', cards: [] });
+            var cards = [];
+            if (callback) {
+                callback(cards);
+                return true;
+            } else {
+                chrome.tabs.sendMessage(sender.tab.id, { msg: 'cards', cards: [] });
+            }
         });
     };
 });
--- test/spec/youtube-video-background.js
+++ test/spec/youtube-video-background.js
@@ -17,13 +17,17 @@ describe('youtube-video-background', function() {
     });
 
     describe('when receiving triggered message', function() {
-        beforeEach(function() {
-            chrome.runtime.onMessage.addListener.yield({ msg: 'triggered', content: 'youtube-video', id: 'SOME_ID' }, { tab: { id: 'TAB_ID' } });
-        });
-
         it('should send cards message', function() {
+            chrome.runtime.onMessage.addListener.yield({ msg: 'triggered', content: 'youtube-video', id: 'SOME_ID' }, { tab: { id: 'TAB_ID' } });
             chrome.tabs.sendMessage.should.have.been.calledWith('TAB_ID', sinon.match.has('msg', 'cards'));
             chrome.tabs.sendMessage.should.have.been.calledWith('TAB_ID', sinon.match.has('cards', sinon.match.array));
+        });
+
+        it('should call callback with cards if callback is provided', function() {
+            var callback = sandbox.spy();
+            chrome.runtime.onMessage.addListener.yield({ msg: 'triggered', content: 'youtube-video', id: 'SOME_ID' }, { tab: { id: 'TAB_ID' } }, callback);
+            chrome.tabs.sendMessage.should.not.have.been.called;
+            callback.should.have.been.calledWith(sinon.match.array);
         });
     });
 });
