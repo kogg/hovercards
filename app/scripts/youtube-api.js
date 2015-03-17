56cc180ab5508d66930b08d03894610988f120d1
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-16 23:36:20 -0700

    Call youtube API for youtube-video

--- app/scripts/youtube-api.js
+++ app/scripts/youtube-api.js
@@ -4,15 +4,22 @@ define(['jquery'], function($) {
     var youtubeApi = {};
 
     function video(id, callback) {
-        $.ajax({
-            url: 'https://www.googleapis.com/youtube/v3/videos',
-            data: {
-                id: id,
-                part: 'snippet,statistics',
-                key: youtubeApi.API_KEY
-            }
-        });
-        callback();
+        $.ajax({ url:  'https://www.googleapis.com/youtube/v3/videos',
+                 data: { id:   id,
+                         part: 'snippet,statistics',
+                         key:  youtubeApi.API_KEY } })
+            .done(function(data) {
+                callback(null, { content:     'youtube-video',
+                                 id:          id,
+                                 icon:        data.items[0].snippet.thumbnails.medium.url,
+                                 title:       data.items[0].snippet.localized.title,
+                                 description: data.items[0].snippet.localized.description,
+                                 date:        Date.parse(data.items[0].snippet.publishedAt),
+                                 views:       data.items[0].statistics.viewCount,
+                                 likes:       data.items[0].statistics.likeCount,
+                                 dislikes:    data.items[0].statistics.dislikeCount,
+                                 channel:     { id: data.items[0].snippet.channelId } });
+            });
     }
 
     function channel(id, callback) {
--- test/spec/youtube-api.js
+++ test/spec/youtube-api.js
@@ -21,15 +21,39 @@ describe('youtube-api', function() {
     });
 
     describe('.video', function() {
-        beforeEach(function(done) {
-            youtubeApi.video('SOME_VIDEO_ID', done);
-        });
-
         it('should make an ajax call', function() {
+            $.ajax.returns({ done: $.noop });
+            youtubeApi.video('SOME_VIDEO_ID', $.noop);
             $.ajax.should.have.been.calledWith(sinon.match.has('url', 'https://www.googleapis.com/youtube/v3/videos'));
             $.ajax.should.have.been.calledWith(sinon.match.has('data', sinon.match.has('id', 'SOME_VIDEO_ID')));
             $.ajax.should.have.been.calledWith(sinon.match.has('data', sinon.match.has('part', 'snippet,statistics')));
             $.ajax.should.have.been.calledWith(sinon.match.has('data', sinon.match.has('key', youtubeApi.API_KEY)));
+        });
+
+        it('should return a youtubeVideoCard', function() {
+            $.ajax.returns({
+                done: sandbox.stub().yields({ items: [{ snippet:    { publishedAt: '2011-04-06T03:21:59.000Z',
+                                                                      channelId:   'SOME_CHANNEL_ID',
+                                                                      thumbnails:  { medium: { url: 'medium_url.jpg' } },
+                                                                      localized:   { title:       'Some Title',
+                                                                                     description: 'Some Description' } },
+                                                        statistics: { viewCount:    1000,
+                                                                      likeCount:    2000,
+                                                                      dislikeCount: 3000 } }] })
+            });
+            var callback = sandbox.spy();
+            youtubeApi.video('SOME_VIDEO_ID', callback);
+            callback.should.have.been.calledWith(null);
+            callback.should.have.been.calledWith(sinon.match.any, sinon.match.has('content',     'youtube-video'));
+            callback.should.have.been.calledWith(sinon.match.any, sinon.match.has('id',          'SOME_VIDEO_ID'));
+            callback.should.have.been.calledWith(sinon.match.any, sinon.match.has('icon',        'medium_url.jpg'));
+            callback.should.have.been.calledWith(sinon.match.any, sinon.match.has('title',       'Some Title'));
+            callback.should.have.been.calledWith(sinon.match.any, sinon.match.has('description', 'Some Description'));
+            callback.should.have.been.calledWith(sinon.match.any, sinon.match.has('date',        1302060119000));
+            callback.should.have.been.calledWith(sinon.match.any, sinon.match.has('views',       1000));
+            callback.should.have.been.calledWith(sinon.match.any, sinon.match.has('likes',       2000));
+            callback.should.have.been.calledWith(sinon.match.any, sinon.match.has('dislikes',    3000));
+            callback.should.have.been.calledWith(sinon.match.any, sinon.match.has('channel',     sinon.match.has('id', 'SOME_CHANNEL_ID')));
         });
     });
 
