1fa3c9f0be4bc9f9c2b280f7c2d75f50bbff5a61
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-17 20:57:10 -0700

    description replaces urls and newlines with links and linebreaks

--- app/scripts/cards-controller.js
+++ app/scripts/cards-controller.js
@@ -1,13 +1,19 @@
 'use strict';
 
 define(['angular-app'], function(app) {
-    app.controller('CardsController', ['$scope', function($scope) {
+    app.controller('CardsController', ['$scope', '$filter', function($scope, $filter) {
         $scope.cards = [];
         chrome.runtime.onMessage.addListener(function(request) {
             if (request.msg !== 'cards') {
                 return;
             }
             $scope.$apply(function() {
+                for (var i = 0; i < request.cards.length; i++) {
+                    if (!request.cards[i].description) {
+                        continue;
+                    }
+                    request.cards[i].description = $filter('linky')(request.cards[i].description, '_blank').replace(/(&#10;|\n)/g, '<br>');
+                }
                 $scope.cards = request.cards;
             });
         });
