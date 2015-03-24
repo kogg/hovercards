10633346bca99e3df6397e3f2de4857745be8831
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-24 16:12:38 -0700

    Injector skeleton in place

---
+++ app/scripts/injector.js
@@ -0,0 +1,15 @@
+'use strict';
+
+define('injector', [], function() {
+    var injector = {};
+
+    function register() {
+    }
+    injector.register = register;
+
+    function inject() {
+    }
+    injector.inject = inject;
+
+    return injector;
+});
--- test/spec/injector.js
+++ test/spec/injector.js
@@ -1,5 +1,24 @@
 'use strict';
 
 describe('injector', function() {
-    it('should have tests');
+    var injector;
+
+    beforeEach(function(done) {
+        require(['injector'], function(_injector) {
+            injector = _injector;
+            done();
+        });
+    });
+
+    describe('#register', function() {
+        it('should be a function', function() {
+            injector.register.should.be.a('function');
+        });
+    });
+
+    describe('#inject', function() {
+        it('should be a function', function() {
+            injector.inject.should.be.a('function');
+        });
+    });
 });
