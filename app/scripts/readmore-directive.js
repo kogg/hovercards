db7692c8eca7910704950d9d74a5c2bf9f8ededb
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-26 20:49:24 -0700

    Use max-height for the readmore things now

--- app/scripts/readmore-directive.js
+++ app/scripts/readmore-directive.js
@@ -5,12 +5,9 @@ define(['angular-app', 'angular-sanitize', 'dotdotdot'], function(app) {
         return {
             restrict: 'A',
             scope: {
-                height: '@cutoffHeight',
                 text: '=readmore'
             },
             link: function($scope, $element) {
-                var previousMaxHeight = $element.css('max-height');
-                $element.css('max-height', $scope.height);
                 $element.html($sanitize($scope.text + ' <span class="read-more">Read More</span>'));
                 $element.dotdotdot({
                     after: 'span.read-more',
@@ -23,7 +20,7 @@ define(['angular-app', 'angular-sanitize', 'dotdotdot'], function(app) {
                         $element.append(link); // FIXME Hack AF https://github.com/BeSite/jQuery.dotdotdot/issues/67
                         link.click(function() {
                             $element.trigger('destroy');
-                            $element.css('max-height', previousMaxHeight);
+                            $element.css('max-height', 'none');
                             $element.html($sanitize($scope.text));
                         });
                     }
