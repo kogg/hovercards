2ee47ae60c668117fd8b4749110d5667631540fa
Cameron Rohani <cameron.rohani@gmail.com>
2015-03-27 03:06:00 -0700

    fix quotes

--- app/scripts/supportkit.js
+++ app/scripts/supportkit.js
@@ -4,9 +4,9 @@
 /* jshint unused:false */
 require(['jquery'], function($) {
     SupportKit.init({appToken: '4yfkgr5329ya65cuetj7weiyo'});
-		$(".feedback-trigger").click(function(){
-		    $("#sk-container").show();
+		$('.feedback-trigger').click(function(){
+		    $('#sk-container').show();
 		    $(this).hide();
-		    $("body").addClass('reduce-padding');
+		    $('body').addClass('reduce-padding');
 		});
 });
