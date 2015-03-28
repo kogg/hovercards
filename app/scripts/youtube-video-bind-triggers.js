7360e1a0615c36867c6ad936c96ca06ef54c3fd6
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-28 06:44:43 -0700

    youtu.be links

--- app/scripts/youtube-video-bind-triggers.js
+++ app/scripts/youtube-video-bind-triggers.js
@@ -7,6 +7,10 @@ define('youtube-video-bind-triggers', ['jquery', 'hover-trigger'], function($, h
             /* globals purl:true */
             return purl($(this).attr('href')).param('v');
         });
+        hoverTrigger.handle(body, 'youtube-video', 'a[href*="youtu.be/"]', function() {
+            /* globals purl:true */
+            return purl($(this).prop('href')).segment(-1);
+        });
         hoverTrigger.handle(body, 'youtube-video', 'embed[src*="youtube.com/v/"]', function() {
             /* globals purl:true */
             return purl($(this).prop('src')).segment(-1);
