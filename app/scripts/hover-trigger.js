6ada3b9efcab20810ffb26317aa9139bedcf1c52
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-28 02:52:57 -0700

    hover-trigger ready

--- app/scripts/hover-trigger.js
+++ app/scripts/hover-trigger.js
@@ -1,5 +1,23 @@
 'use strict';
 
 define('hover-trigger', ['jquery'], function($) {
-    return { handle: $.noop };
+    return {
+        handle: function(body, content, selector, getId) {
+            body.on('mouseenter', selector, function() {
+                chrome.runtime.sendMessage({ msg: 'hover', content: content, id: getId.call(this) });
+            });
+            body.on('mouseleave', selector, function() {
+                clearTimeout($(this).data('hovercards-timeout'));
+                chrome.runtime.sendMessage({ msg: 'unhover' });
+            });
+            body.on('mousedown', selector, function() {
+                $(this).data('hovercards-timeout', setTimeout(function() {
+                    chrome.runtime.sendMessage({ msg: 'activate', content: content, id: getId.call(this) });
+                }, 333));
+            });
+            body.on('click', selector, function() {
+                clearTimeout($(this).data('hovercards-timeout'));
+            });
+        }
+    };
 });
---
+++ test/spec/hover-trigger.js
@@ -0,0 +1,64 @@
+'use strict';
+
+describe('hover-trigger', function() {
+    var sandbox = sinon.sandbox.create();
+    var body;
+    var link;
+    var hoverTrigger;
+
+    beforeEach(function(done) {
+        require(['hover-trigger'], function(_hoverTrigger) {
+            sandbox.useFakeTimers();
+            sandbox.stub(chrome.runtime, 'sendMessage');
+            hoverTrigger = _hoverTrigger;
+            done();
+        });
+    });
+
+    beforeEach(function() {
+        body = $('<div id="sandbox"></div>');
+        link = $('<a id="link" href="SOME_URL"></a>').appendTo(body);
+        hoverTrigger.handle(body, 'something', '#link', function() {
+            return 'SOME_ID';
+        });
+    });
+
+    afterEach(function() {
+        sandbox.restore();
+        body.remove();
+    });
+
+    describe('hover/unhover', function() {
+        it('should send msg:hover on mouseenter', function() {
+            link.mouseenter();
+            expect(chrome.runtime.sendMessage).to.have.been.calledWith({ msg: 'hover', content: 'something', id: 'SOME_ID' });
+        });
+
+        it('should send msg:unhover on mouseleave', function() {
+            link.mouseleave();
+            expect(chrome.runtime.sendMessage).to.have.been.calledWith({ msg: 'unhover' });
+        });
+    });
+
+    describe('longpress', function() {
+        it('should send msg:activate mousedown > 333ms', function() {
+            link.mousedown();
+            sandbox.clock.tick(333);
+            expect(chrome.runtime.sendMessage).to.have.been.calledWith({ msg: 'activate', content: 'something', id: 'SOME_ID' });
+        });
+
+        it('should not send msg:activate mousedown > click > 333ms', function() {
+            link.mousedown();
+            link.click();
+            sandbox.clock.tick(333);
+            expect(chrome.runtime.sendMessage).to.not.have.been.calledWith({ msg: 'activate', content: 'something', id: 'SOME_ID' });
+        });
+
+        it('should not send msg:activate mousedown > mouseleave > 333ms', function() {
+            link.mousedown();
+            link.mouseleave();
+            sandbox.clock.tick(333);
+            expect(chrome.runtime.sendMessage).to.not.have.been.calledWith({ msg: 'activate', content: 'something', id: 'SOME_ID' });
+        });
+    });
+});
