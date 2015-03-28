fab796a22cd06c1237e686a7460655ad508e8f50
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-28 02:05:49 -0700

    finish youtube-video-bind-triggers

--- app/scripts/youtube-video-bind-triggers.js
+++ app/scripts/youtube-video-bind-triggers.js
@@ -3,8 +3,13 @@
 define('youtube-video-bind-triggers', ['jquery', 'purl', 'hover-trigger'], function($, purl, hoverTrigger) {
     return function(body) {
         hoverTrigger.handle(body, 'youtube-video', 'a[href*="youtube.com/watch"]', function() {
-            var link = $(this);
-            return purl(link.attr('href')).param('v');
+            return purl($(this).attr('href')).param('v');
+        });
+        hoverTrigger.handle(body, 'youtube-video', 'embed[src*="youtube.com/v/"]', function() {
+            return purl($(this).prop('src')).segment(-1);
+        });
+        hoverTrigger.handle(body, 'youtube-video', 'object[data*="youtube.com/v/"]', function() {
+            return purl($(this).prop('data')).segment(-1);
         });
     };
 });
--- test/spec/youtube-video-bind-triggers.js
+++ test/spec/youtube-video-bind-triggers.js
@@ -45,4 +45,48 @@ describe('youtube-video-bind-triggers', function() {
             expect(args[3].call(element)).to.equal('SOME_ID');
         });
     });
+
+    describe('on embed[src="https://www.youtube.com/v/SOME_ID"]"', function() {
+        var element;
+        var selector;
+
+        beforeEach(function() {
+            element = $('<embed src="https://www.youtube.com/v/SOME_ID">').appendTo(body);
+            youtubeVideoBindTriggers(body);
+        });
+
+        it('should be handled by hoverTrigger', function() {
+            expect(hoverTrigger.handle).to.be.calledWith(body, 'youtube-video', sinon.match(function(_selector) {
+                selector = _selector;
+                return body.find(selector)[0] === element[0];
+            }, 'matches element'));
+        });
+
+        it('should parse ID', function() {
+            var args = hoverTrigger.handle.withArgs(body, 'youtube-video', selector).args[0];
+            expect(args[3].call(element)).to.equal('SOME_ID');
+        });
+    });
+
+    describe('on object[data="https://www.youtube.com/v/SOME_ID"]"', function() {
+        var element;
+        var selector;
+
+        beforeEach(function() {
+            element = $('<object data="https://www.youtube.com/v/SOME_ID"></object>').appendTo(body);
+            youtubeVideoBindTriggers(body);
+        });
+
+        it('should be handled by hoverTrigger', function() {
+            expect(hoverTrigger.handle).to.be.calledWith(body, 'youtube-video', sinon.match(function(_selector) {
+                selector = _selector;
+                return body.find(selector)[0] === element[0];
+            }, 'matches element'));
+        });
+
+        it('should parse ID', function() {
+            var args = hoverTrigger.handle.withArgs(body, 'youtube-video', selector).args[0];
+            expect(args[3].call(element)).to.equal('SOME_ID');
+        });
+    });
 });
