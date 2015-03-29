21c8be61734a65e6e3b771c8362507998f5fbbee
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-28 19:57:54 -0700

    Use state-manager

--- app/scripts/trigger-background.js
+++ app/scripts/trigger-background.js
@@ -2,57 +2,51 @@
 
 define('trigger-background', [], function() {
     return function triggerBackground() {
-        var state = {};
         chrome.runtime.onMessage.addListener(function(request, sender) {
-            var tabId = sender.tab.id;
-            if (!state[tabId]) {
-                state[tabId] = { maybe: null, sent: null };
-            }
-            var tabState = state[tabId];
-            switch (request.msg) {
-                case 'hover':
-                    tabState.maybe = { content: request.content, id: request.id };
-                    var provider = request.content.split('-')[0];
-                    if (tabState.ready) {
-                        chrome.pageAction.setIcon({ tabId: tabId,
-                                                    path:  { '19': 'images/omni-' + provider + '-19.png',
-                                                             '38': 'images/omni-' + provider + '-38.png' } });
-                    }
-                    break;
-                case 'unhover':
-                    tabState.maybe = null;
-                    if (tabState.ready) {
-                        chrome.pageAction.setIcon({ tabId: tabId,
-                                                    path:  { '19': 'images/omni-default-19.png',
-                                                             '38': 'images/omni-default-38.png' } });
-                    }
-                    break;
-                case 'activate':
-                    var current = (request.content && { content: request.content, id: request.id }) || tabState.maybe;
-                    tabState.maybe = null;
-                    if (!current || (tabState.sent && tabState.sent.content === current.content && tabState.sent.id === current.id)) {
-                        chrome.tabs.sendMessage(tabId, { msg: 'hide' });
-                        current = null;
-                    } else if (tabState.ready) {
-                        chrome.tabs.sendMessage(tabId, { msg: 'load', content: current.content, id: current.id });
-                    }
-                    tabState.sent = current;
-                    break;
-                case 'ready':
-                    tabState.ready = true;
-                    chrome.pageAction.show(tabId);
-                    if (tabState.sent) {
-                        chrome.tabs.sendMessage(tabId, { msg: 'load', content: tabState.sent.content, id: tabState.sent.id });
-                    }
-                    break;
-            }
-        });
-
-        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
-            if (changeInfo.status !== 'loading') {
+            if (request.msg !== 'hover' && request.msg !== 'unhover' && request.msg !== 'activate' && request.msg !== 'ready') {
                 return;
             }
-            state[tabId] = { maybe: null, sent: null };
+            var tabId = sender.tab.id;
+            chrome.tabs.sendMessage(tabId, { msg: 'getstate' }, function(state) {
+                switch (request.msg) {
+                    case 'hover':
+                        state.maybe = { content: request.content, id: request.id };
+                        var provider = request.content.split('-')[0];
+                        if (state.ready) {
+                            chrome.pageAction.setIcon({ tabId: tabId,
+                                                        path:  { '19': 'images/omni-' + provider + '-19.png',
+                                                                 '38': 'images/omni-' + provider + '-38.png' } });
+                        }
+                        break;
+                    case 'unhover':
+                        state.maybe = null;
+                        if (state.ready) {
+                            chrome.pageAction.setIcon({ tabId: tabId,
+                                                        path:  { '19': 'images/omni-default-19.png',
+                                                                 '38': 'images/omni-default-38.png' } });
+                        }
+                        break;
+                    case 'activate':
+                        var current = (request.content && { content: request.content, id: request.id }) || state.maybe;
+                        state.maybe = null;
+                        if (!current || (state.sent && state.sent.content === current.content && state.sent.id === current.id)) {
+                            chrome.tabs.sendMessage(tabId, { msg: 'hide' });
+                            current = null;
+                        } else if (state.ready) {
+                            chrome.tabs.sendMessage(tabId, { msg: 'load', content: current.content, id: current.id });
+                        }
+                        state.sent = current;
+                        break;
+                    case 'ready':
+                        state.ready = true;
+                        chrome.pageAction.show(tabId);
+                        if (state.sent) {
+                            chrome.tabs.sendMessage(tabId, { msg: 'load', content: state.sent.content, id: state.sent.id });
+                        }
+                        break;
+                }
+                chrome.tabs.sendMessage(tabId, { msg: 'setstate', state: state });
+            });
         });
     };
 });
