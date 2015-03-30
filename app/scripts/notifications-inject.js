184f27737340a22b687ff8e1c504534135ba0487
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-30 16:29:25 -0700

    Notifications are in a container and each notification removes itself after 15 seconds

--- app/scripts/notifications-inject.js
+++ app/scripts/notifications-inject.js
@@ -4,18 +4,22 @@ define('notifications-inject', [], function() {
     return {
         on: function notificationsInjectOn(body) {
             body = $(body);
+            var notifications = $('<div class="hovercards-notifications-container"></div>')
+                .appendTo(body);
+
             chrome.runtime.onMessage.addListener(function(request) {
                 if (request.msg !== 'notification') {
                     return;
                 }
-                $('<div class="hovercards-notification"></div>')
-                    .appendTo(body)
+                var notification = $('<div class="hovercards-notification"></div>')
+                    .appendTo(notifications)
                     .append('<div class="hovercards-notification-image hovercards-notification-yt"></div>')
                     .append('<div class="hovercards-notification-text"><p>You just hovered over a Hover Cards Link! <b>Click and hold</b> it, or <b>press shift</b> while hovering over it to activate Hover Cards!</p></div>')
                     .click(function(e) {
                         if (e.which !== 1) {
                             return;
                         }
+                        clearTimeout(timeout);
                         $(this).addClass('hovercards-notification-exit-animation');
                     })
                     .on('animationend MSAnimationEnd webkitAnimationEnd oAnimationEnd', function(e) {
@@ -24,6 +28,10 @@ define('notifications-inject', [], function() {
                         }
                         $(this).remove();
                     });
+
+                var timeout = setTimeout(function() {
+                    notification.addClass('hovercards-notification-exit-animation');
+                }, 15000);
             });
         }
     };
--- test/spec/notifications-inject.js
+++ test/spec/notifications-inject.js
@@ -6,6 +6,7 @@ describe('notifications-inject', function() {
 
     beforeEach(function(done) {
         require(['notifications-inject'], function(notificationsInject) {
+            sandbox.useFakeTimers();
             sandbox.stub(chrome.runtime.onMessage, 'addListener');
             body = $('<div id="sandbox"></div>');
             notificationsInject.on(body);
@@ -18,19 +19,29 @@ describe('notifications-inject', function() {
         body.remove();
     });
 
-    it('should not make a notification initially', function() {
-        expect(body.find('.hovercards-notification')).to.not.exist;
+    it('should make a notification container', function() {
+        expect(body.children('.hovercards-notifications-container')).to.exist;
     });
 
-    it('should create a card on notification', function() {
-        chrome.runtime.onMessage.addListener.yield({ msg: 'notification', which: 'something' });
-        expect(body.find('.hovercards-notification')).to.exist;
-    });
+    describe('a notification', function() {
+        it('should create a card on notification', function() {
+            chrome.runtime.onMessage.addListener.yield({ msg: 'notification', which: 'something' });
+            var element = body.children('.hovercards-notifications-container').children('.hovercards-notification');
+            expect(element).to.exist;
+        });
 
-    it('should add .hovercards-notification-exit-animation on click', function() {
-        chrome.runtime.onMessage.addListener.yield({ msg: 'notification', which: 'something' });
-        var element = body.find('.hovercards-notification');
-        element.trigger($.Event('click', { which: 1 }));
-        expect(element).to.match('.hovercards-notification-exit-animation');
+        it('should add .hovercards-notification-exit-animation on click', function() {
+            chrome.runtime.onMessage.addListener.yield({ msg: 'notification', which: 'something' });
+            var element = body.children('.hovercards-notifications-container').children('.hovercards-notification');
+            element.trigger($.Event('click', { which: 1 }));
+            expect(element).to.match('.hovercards-notification-exit-animation');
+        });
+
+        it('should add .hovercards-notification-exit-animation after 15s', function() {
+            chrome.runtime.onMessage.addListener.yield({ msg: 'notification', which: 'something' });
+            var element = body.children('.hovercards-notifications-container').children('.hovercards-notification');
+            sandbox.clock.tick(15000);
+            expect(element).to.match('.hovercards-notification-exit-animation');
+        });
     });
 });
