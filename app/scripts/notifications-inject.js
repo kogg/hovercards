00c46c0d4d94cbbe7af057ce32eca2c3f994aae8
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-30 06:49:16 -0700

    Placeholder text

--- app/scripts/notifications-inject.js
+++ app/scripts/notifications-inject.js
@@ -8,8 +8,10 @@ define('notifications-inject', [], function() {
                 if (request.msg !== 'notification') {
                     return;
                 }
-                $('<div class="hovercards-notification"><h1>LOOK AT ME LOOK AT ME</h1></div>')
+                $('<div class="hovercards-notification"></div>')
                     .appendTo(body)
+                    .append('<div class="hovercards-notification-image hovercards-notification-yt"></div>')
+                    .append('<div class="hovercards-notification-text">Some Text for now</div>')
                     .click(function(e) {
                         if (e.which !== 1) {
                             return;
