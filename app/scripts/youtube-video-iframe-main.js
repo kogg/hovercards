c2ad7eba6e8145712028b44cc06ca55b52e9c334
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-29 23:28:01 -0700

    youtube-videos are better lol

--- app/scripts/youtube-video-iframe-main.js
+++ app/scripts/youtube-video-iframe-main.js
@@ -1,8 +1,12 @@
 'use strict';
 
 require(['hover-trigger'], function(hoverTrigger) {
-    hoverTrigger.handle('body', 'youtube-video', '#player', function() {
+    hoverTrigger.handle('body', 'youtube-video', '#player .html5-video-container', function() {
         /* globals purl:true */
         return purl(document.URL).segment(-1);
     });
+});
+
+require(['youtube-video-bind-triggers'], function(youtubeVideoBindTriggers) {
+    youtubeVideoBindTriggers('#player .html5-info-bar');
 });
