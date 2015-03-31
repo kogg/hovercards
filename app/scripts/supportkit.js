cabe5cb5e854c79820c2bce7c5a15f126823fe44
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-31 05:49:25 -0700

    SupportKit ain't got nutin on deese nutz

--- app/scripts/supportkit.js
+++ app/scripts/supportkit.js
@@ -4,15 +4,26 @@
 /* jshint unused:false */
 require(['jquery'], function($) {
     SupportKit.init({appToken: '4yfkgr5329ya65cuetj7weiyo'});
-		$('.feedback-trigger').click(function(){
-		    $('#sk-container').show();
-		    $(this).hide();
-		    $('body').addClass('reduce-padding');
-				$('#sk-footer form input').focus();
-		    $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
-				return false;
-		});
-		$('body').on('click', '#sk-container', function(){
-			$('#sk-footer form input').focus();
-		});
+
+    $('.feedback-trigger').click(function(){
+        $('#sk-container').show();
+        $(this).hide();
+        $('body').addClass('reduce-padding');
+        $('#sk-footer form input').focus();
+        $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
+        return false;
+    });
+
+    $('body').on('click', '#sk-container', function(){
+        $('#sk-footer form input').focus();
+    });
+
+    $('body').on('keydown', function(e) {
+        if (e.which !== 16) {
+            return;
+        }
+        if ($(e.originalEvent.target).is('#sk-footer form input')) {
+            e.stopImmediatePropagation();
+        }
+    });
 });
