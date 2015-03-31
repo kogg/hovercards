173ece4e9a7f9745a684f9c20a1a8df8c8b18437
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-31 04:14:24 -0700

    Trying out a card

--- app/scripts/sidebar-main.js
+++ app/scripts/sidebar-main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require(['layout-directive',
-         'content-directive',
+         'card-directive',
          'readmore-directive',
          'youtube-channel-subscribe-directive',
          'htmlify-filter',
