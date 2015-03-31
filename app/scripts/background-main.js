e5c62af63a065a3af0bed07820c3d1e9747287dc
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-03-31 02:18:20 -0700

    Lazy omnibar implementation

--- app/scripts/background-main.js
+++ app/scripts/background-main.js
@@ -8,6 +8,10 @@ require(['notifications-background'], function(notificationsBackground) {
     notificationsBackground.init();
 });
 
+require(['omnibar-background'], function(omnibarBackground) {
+    omnibarBackground.init();
+});
+
 require(['youtube-background'], function(youtubeBackground) {
     youtubeBackground.init();
 });
