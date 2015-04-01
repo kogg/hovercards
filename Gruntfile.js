fa31d7e42f3bd1b9682eb895776fc1ffb52eb00f
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-04-01 05:15:07 -0700

    Some more trimming

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,10 @@
-// Generated on 2015-02-25 using generator-chrome-extension 0.3.0
 'use strict';
 
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
-
 module.exports = function (grunt) {
-
-    // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
-
-    // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
 
-    // Configurable paths
-    var config = {
-        app: 'app',
-        dist: 'dist'
-    };
-
     grunt.initConfig({
-
-        // Project settings
-        config: config,
-
-        // Grunt server and debug server setting
-        connect: {
-            options: {
-                livereload: 35729,
-                // change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
-            },
-            test: {
-                options: {
-                    port: 9500,
-                    open: false,
-                    base: [
-                        'test',
-                        '<%= config.app %>'
-                    ]
-                }
-            },
-            testBrowser: {
-                options: {
-                    port: 9750,
-                    open: true,
-                    keepalive: true,
-                    base: [
-                        'test',
-                        '<%= config.app %>'
-                    ]
-                }
-            }
-        },
-
-        // Make sure code styles are up to par and there are no obvious mistakes
         jshint: {
             options: {
                 jshintrc: true,
@@ -64,9 +12,29 @@ module.exports = function (grunt) {
             },
             all: [
                 'Gruntfile.js',
-                '<%= config.app %>/scripts/{,*/}*.js',
+                'app/scripts/{,*/}*.js',
                 'test/spec/{,*/}*.js'
             ]
+        },
+        connect: {
+            options: {
+                hostname: 'localhost'
+            },
+            test: {
+                options: {
+                    port: 9500,
+                    open: false,
+                    base: ['test', 'app']
+                }
+            },
+            testBrowser: {
+                options: {
+                    port: 9750,
+                    open: true,
+                    keepalive: true,
+                    base: ['test', 'app']
+                }
+            }
         },
         mocha: {
             all: {
