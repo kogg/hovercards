e6d337de908c7757cf461f037e8dd820d9333760
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-04-04 19:06:12 -0700

    Testing the cards better

--- test/spec/cards-directive.js
+++ test/spec/cards-directive.js
@@ -53,17 +53,17 @@ describe('cards-directive', function() {
             sandbox.server.respondWith('https://hovercards.herokuapp.com/v1/cards?triggers[0][type]=somewhere-something&triggers[0][query_type]=id&triggers[0][query]=SOME_ID',
                                        [200,
                                         { 'Content-Type': 'application/json' },
-                                        JSON.stringify({ provider: 'somewhere-1', content: 'something-1', id: 'SOME_ID-1'}) + '\n' +
-                                        JSON.stringify({ provider: 'somewhere-2', content: 'something-2', id: 'SOME_ID-2'}) + '\n' +
-                                        JSON.stringify({ provider: 'somewhere-3', content: 'something-3', id: 'SOME_ID-3'}) + '\n']);
+                                        JSON.stringify({ type: 'somewhere-something-1', network: 'somewhere', id: 'SOME_ID-1'}) + '\n' +
+                                        JSON.stringify({ type: 'somewhere-something-2', network: 'somewhere', id: 'SOME_ID-2'}) + '\n' +
+                                        JSON.stringify({ type: 'somewhere-something-3', network: 'somewhere', id: 'SOME_ID-3'}) + '\n']);
             var scope = element.isolateScope();
             chrome.runtime.onMessage.addListener.yield({ msg: 'load', provider: 'somewhere', content: 'something', id: 'SOME_ID' });
             sandbox.server.respond();
             $rootScope.$digest();
 
-            expect(scope.cardsets).to.deep.equal([[{ provider: 'somewhere-1', content: 'something-1', id: 'SOME_ID-1'},
-                                                   { provider: 'somewhere-2', content: 'something-2', id: 'SOME_ID-2'},
-                                                   { provider: 'somewhere-3', content: 'something-3', id: 'SOME_ID-3'}]]);
+            expect(scope.cardsets).to.deep.equal([[{ type: 'somewhere-something-1', network: 'somewhere', id: 'SOME_ID-1'},
+                                                   { type: 'somewhere-something-2', network: 'somewhere', id: 'SOME_ID-2'},
+                                                   { type: 'somewhere-something-3', network: 'somewhere', id: 'SOME_ID-3'}]]);
         });
     });
 });
