6d4d3958205832668c1f81b078111a0c980f6d6f
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-04-06 20:27:31 -0700

    trigger-inject uses hover_trigger on everything but youtube videos themselves

--- app/scripts/trigger-inject.js
+++ app/scripts/trigger-inject.js
@@ -8,6 +8,18 @@ define('trigger-inject', ['jquery', 'hover-trigger'], function($, hover_trigger)
             hover_trigger.on(body, 'a[href]', function(link) {
                 return link.attr('href');
             });
+
+            hover_trigger.on(body, 'a[data-href]', function(link) {
+                return link.data('href');
+            });
+
+            hover_trigger.on(body, 'embed[src]', function(embed) {
+                return embed.attr('src');
+            });
+
+            hover_trigger.on(body, 'object[data]', function(object) {
+                return object.attr('data');
+            });
         }
     };
 });
--- test/spec/trigger-inject.js
+++ test/spec/trigger-inject.js
@@ -23,16 +23,70 @@ describe('trigger-inject', function() {
     });
 
     it('should bind to a[href]', function() {
+        var obj = $('<a href="URL">Some Link</a>').appendTo(body);
         trigger_inject.on(body);
 
         expect(hover_trigger.on).to.be.calledWith(
             sinon.match(function(element) {
                 return body[0] === element[0];
             }, 'body'),
-            'a[href]',
+            sinon.match(function(selector) {
+                return body.find(selector).is(obj);
+            }, 'a[href]'),
             sinon.match(function(func) {
-                return func($('<a href="SOME_HREF">Some Link</a>')) === 'SOME_HREF';
+                return func(obj) === 'URL';
             }, 'func that gets href')
+        );
+    });
+
+    it('should bind to a[data-href]', function() {
+        var obj = $('<a data-href="URL">Some Link</a>').appendTo(body);
+        trigger_inject.on(body);
+
+        expect(hover_trigger.on).to.be.calledWith(
+            sinon.match(function(element) {
+                return body[0] === element[0];
+            }, 'body'),
+            sinon.match(function(selector) {
+                return body.find(selector).is(obj);
+            }, 'a[data-href]'),
+            sinon.match(function(func) {
+                return func(obj) === 'URL';
+            }, 'func that gets data-href')
+        );
+    });
+
+    it('should bind to embed[src]', function() {
+        var obj = $('<embed src="URL">').appendTo(body);
+        trigger_inject.on(body);
+
+        expect(hover_trigger.on).to.be.calledWith(
+            sinon.match(function(element) {
+                return body[0] === element[0];
+            }, 'body'),
+            sinon.match(function(selector) {
+                return body.find(selector).is(obj);
+            }, 'embed[src]'),
+            sinon.match(function(func) {
+                return func(obj) === 'URL';
+            }, 'func that gets src')
+        );
+    });
+
+    it('should bind to object[data]', function() {
+        var obj = $('<object data="URL"></object>').appendTo(body);
+        trigger_inject.on(body);
+
+        expect(hover_trigger.on).to.be.calledWith(
+            sinon.match(function(element) {
+                return body[0] === element[0];
+            }, 'body'),
+            sinon.match(function(selector) {
+                return body.find(selector).is(obj);
+            }, 'object[data]'),
+            sinon.match(function(func) {
+                return func(obj) === 'URL';
+            }, 'func that gets data')
         );
     });
 });
