ec6e471523eb99cd1add2b2cbe8aac4e46e2d6e6
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-04-09 19:08:37 -0700

    Everything we inject goes inside of a div I make so we can z-indexes relative to each other

--- app/scripts/top-frame-main.js
+++ app/scripts/top-frame-main.js
@@ -1,11 +1,15 @@
 'use strict';
 
-require(['notifications-inject'], function(notifications_inject) {
-    notifications_inject.on('body');
-});
+require(['jquery'], function($) {
+    var hovercards_container = $('<div class="hovercards-container"></div>').appendTo('body');
 
-require(['sidebar-inject'], function(sidebar_inject) {
-    sidebar_inject.on('body', 'body', 'html');
+    require(['notifications-inject'], function(notifications_inject) {
+        notifications_inject.on(hovercards_container);
+    });
+
+    require(['sidebar-inject'], function(sidebar_inject) {
+        sidebar_inject.on(hovercards_container, 'body', 'html');
+    });
 });
 
 require(['state-manager'], function(state_manager) {
