7a86e88b1537c78bf20d034c670868c8f3556e76
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-04-11 17:15:38 -0700

    Using new sidebar and old templates

--- app/scripts/content-directive.js
+++ app/scripts/content-directive.js
@@ -17,7 +17,17 @@ define('content-directive', ['angular-app'], function(app) {
                     }
                     timeout = setTimeout(function() {
                         $scope.$apply(function() {
-                            $scope.content = { type: 'youtube-video', id: 'm3lF2qEA2cw', accounts: [{ type: 'youtube-channel', id: 'UCORIeT1hk6tYBuntEXsguLg' }] };
+                            $scope.content = { type:        'youtube-video',
+                                               id:          'm3lF2qEA2cw',
+                                               name:        'Creep - Vintage Postmodern Jukebox Radiohead Cover ft. Haley Reinhart',
+                                               description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                                               image:       'https://i.ytimg.com/vi/m3lF2qEA2cw/mqdefault.jpg',
+                                               date:        1428437477000,
+                                               views:       1109017,
+                                               likes:       28125,
+                                               dislikes:    384,
+                                               accounts:    [{ type: 'youtube-channel',
+                                                               id:   'UCORIeT1hk6tYBuntEXsguLg' }] };
                         });
                     }, 333);
                 });
