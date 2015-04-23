37c1e048292f8a26703c924fb03904c4d1489e57
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-04-22 20:48:33 -0700

    picker thing works

--- app/scripts/copy-filter.js
+++ app/scripts/copy-filter.js
@@ -3,7 +3,10 @@
 define(['angular-app'], function(app) {
     return app.filter('copy', function() {
         return function(messagename) {
-            return chrome.i18n.getMessage(messagename.replace(/\-/g, '_'));
+            if (!messagename) {
+                return messagename;
+            }
+            return chrome.i18n.getMessage(messagename.replace(/\-/g, '_')) || messagename;
         };
     });
 });
