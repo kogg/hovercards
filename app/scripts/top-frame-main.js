97b4c50d1809b7793f1c5f9f2e51af596950f8fd
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-04-24 21:49:52 -0700

    kill state-manager

--- app/scripts/top-frame-main.js
+++ app/scripts/top-frame-main.js
@@ -2,13 +2,10 @@ var $ = require('jquery');
 
 var notifications_inject = require('./notifications-inject');
 var sidebar_inject       = require('./sidebar-inject');
-var state_manager        = require('./state-manager');
 
 var hovercards_container = $('<div class="hovercards-container"></div>').appendTo('html');
 notifications_inject(hovercards_container);
 sidebar_inject(hovercards_container, 'body', 'html');
-
-state_manager();
 
 var sidebar_trigger = require('./sidebar-trigger')();
 var sidebar_frame;
