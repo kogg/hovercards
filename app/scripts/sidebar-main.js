fe09c7ed4d89550e199158113917e287eea89ec5
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-04-24 21:17:16 -0700

    entry-directive uses window event listener

--- app/scripts/sidebar-main.js
+++ app/scripts/sidebar-main.js
@@ -4,7 +4,3 @@ var angular = require('angular');
 angular.bootstrap(document, [require('./angular-app').name]);
 
 window.top.postMessage({ msg: 'ready' }, '*');
-
-window.addEventListener('message', function(event) {
-    console.log(event.data);
-}, false);
