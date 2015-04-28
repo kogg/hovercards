4da1450d06ab5565d4742c70fd05ca179f81db1c
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-04-28 14:53:38 -0700

    Moving some common stuff into common.js

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -6,3 +6,21 @@ exports.is_fullscreen = function(element) {
     var dom = document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement;
     return dom && element.is(dom);
 };
+
+exports.nullify_bad_url = function(url) {
+    if (url.match(/^javascript:.*/)) {
+        return null;
+    }
+    return url;
+};
+
+exports.relative_to_absolute = function(url) {
+    var a = document.createElement('a');
+    a.href = url;
+    url = a.href;
+    a.href = '';
+    if (a.remove) {
+        a.remove();
+    }
+    return url;
+};
--- app/scripts/everywhere-main.js
+++ app/scripts/everywhere-main.js
@@ -1,4 +1,5 @@
 var $                 = require('jquery');
+var common            = require('./common');
 var embedded_trigger  = require('./embedded-trigger');
 var longpress_trigger = require('./longpress-trigger');
 
@@ -6,40 +7,22 @@ function sendMessage(msg) {
     window.top.postMessage(msg, '*');
 }
 
-function nullify_bad_url(url) {
-    if (url.match(/^javascript:.*/)) {
-        return null;
-    }
-    return url;
-}
-
-function relative_to_absolute(url) {
-    var a = document.createElement('a');
-    a.href = url;
-    url = a.href;
-    a.href = '';
-    if (a.remove) {
-        a.remove();
-    }
-    return url;
-}
-
 var html = $('html');
 
 longpress_trigger(html, 'a[href]', function(link) {
-    return nullify_bad_url(relative_to_absolute(link.attr('href')));
+    return common.nullify_bad_url(common.relative_to_absolute(link.attr('href')));
 }, sendMessage);
 
 longpress_trigger(html, 'a[data-href]', function(link) {
-    return nullify_bad_url(relative_to_absolute(link.data('href')));
+    return common.nullify_bad_url(common.relative_to_absolute(link.data('href')));
 }, sendMessage);
 
 embedded_trigger(html, 'embed[src]', function(embed) {
-    return nullify_bad_url(relative_to_absolute(embed.attr('src')));
+    return common.nullify_bad_url(common.relative_to_absolute(embed.attr('src')));
 }, sendMessage);
 
 embedded_trigger(html, 'object[data]', function(object) {
-    return nullify_bad_url(relative_to_absolute(object.attr('data')));
+    return common.nullify_bad_url(common.relative_to_absolute(object.attr('data')));
 }, sendMessage);
 
 embedded_trigger(html, 'div#player div.html5-video-player', function() {
