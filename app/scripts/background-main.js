d662cff141075e4329e24a569fa8c2da97606913
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-05-04 16:11:29 -0700

    Set (and save) device_id and use it in requests

--- app/scripts/background-main.js
+++ app/scripts/background-main.js
@@ -1,22 +1,36 @@
 var $ = require('jquery');
 
-var client_side_calls = {
-    reddit: require('YoCardsAPICalls/reddit')({ key: 'fNtoQI4_wDq21w' })
-};
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendMessage) {
-    if (client_side_calls[request.api]) {
-        client_side_calls[request.api][request.type](request, function(err, result) {
-            sendMessage([err, result]);
-        });
-        return true;
+chrome.storage.local.get('user_id', function(obj) {
+    if (!obj.user_id) {
+        obj.user_id = (Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2)).substr(0, 25);
+        chrome.storage.local.set(obj);
     }
-    $.get('https://' + chrome.i18n.getMessage('app_short_name') + '.herokuapp.com/v1/' + request.api + '/' + request.type, request)
-        .done(function(data) {
-            sendMessage([null, data]);
-        })
-        .fail(function(err) {
-            sendMessage([err]);
-        });
-    return true;
+
+    var client_side_calls = {
+        reddit: require('YoCardsAPICalls/reddit')({ key: 'fNtoQI4_wDq21w', user: obj.user_id })
+    };
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendMessage) {
+        request = $.extend({}, request);
+        var api  = request.api;
+        var type = request.type;
+        delete request.api;
+        delete request.type;
+
+        if (client_side_calls[api] && client_side_calls[api][type]) {
+            client_side_calls[api][type](request, function(err, result) {
+                sendMessage([err, result]);
+            });
+        } else {
+            request.user = obj.user_id;
+            $.get('https://' + chrome.i18n.getMessage('app_short_name') + '.herokuapp.com/v1/' + api + '/' + type, request)
+                .done(function(data) {
+                    sendMessage([null, data]);
+                })
+                .fail(function(err) {
+                    sendMessage([err]);
+                });
+        }
+        return true;
+    });
 });
