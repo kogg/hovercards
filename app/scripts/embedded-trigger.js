0bb01dab1e1e8bebce0f3df3e4173485c3b83a9e
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-05-06 21:03:52 -0700

    leaving the object (& trigger) removes the trigger entirely

--- app/scripts/embedded-trigger.js
+++ app/scripts/embedded-trigger.js
@@ -14,7 +14,7 @@ module.exports = function(body, selector, get_url) {
             clearTimeout(trigger.data('yo-trigger-timeout'));
             trigger.data('yo-trigger-timeout', setTimeout(function() {
                 trigger.addClass('yo-notify-exit');
-            }, 2000));
+            }, 3000));
             return;
         }
 
@@ -57,67 +57,21 @@ module.exports = function(body, selector, get_url) {
             }));
         trigger.data('yo-trigger-timeout', setTimeout(function() {
             trigger.addClass('yo-notify-exit');
-        }, 2000));
+        }, 3000));
     });
-    /*
-    var trigger = body.find('.hovercards-embedded-trigger');
-    if (!trigger.length) { // FIXME I don't like this at all
-        trigger = $('<div class="hovercards-embedded-trigger"></div>')
-            .appendTo(body)
-            .hide()
-            .mouseenter(function() {
-                trigger.show();
-            })
-            .mouseleave(function(e) {
-                var to_element = e.toElement || e.relatedTarget;
-                if (to_element) { // TODO Unit Test this
-                    to_element = $(to_element);
-                    var obj = trigger.data('hovercards-obj'); // FIXME I REALLY DON'T LIKE THIS
-                    if (obj.is(to_element) || obj.find(to_element).length) {
-                        return;
-                    }
-                }
-                trigger.hide();
-            })
-            .click(function() {
-                trigger.trigger('yo', [trigger.data('hovercards-url')]);
-            });
-        longpress(body, 'div.hovercards-embedded-trigger', function() {
-            return trigger.data('hovercards-url');
-        });
-    }
-    body.on('webkitfullscreenchange mozfullscreenchange fullscreenchange MSFullscreenChange', selector, function() {
-        if (common.is_fullscreen($(this))) {
-            trigger.show();
-            trigger.offset(offset);
-        } else {
-            trigger.hide();
-        }
-    });
-    body.on('mouseenter', selector, function() {
+
+    body.on('mouseleave', selector, function (e) {
         var obj = $(this);
-        var url = get_url(obj);
-        if (!url) {
+        if (!obj.data('yo-trigger')) {
             return;
         }
-        if (trigger.is(':hidden')) {
-            trigger.show();
-            var obj_offset = obj.offset();
-            trigger
-                .offset({ top: obj_offset.top + offset.top, left: obj_offset.left + offset.left })
-                .data('hovercards-url', url)
-                .data('hovercards-obj', obj); // FIXME I REALLY DON'T LIKE THIS
-        }
-    });
-    body.on('mouseleave', selector, function(e) {
+        var trigger = obj.data('yo-trigger');
         var to_element = e.toElement || e.relatedTarget;
-        if (to_element) { // TODO Unit Test this
-            to_element = $(to_element);
-            if (trigger.is(to_element) || trigger.find(to_element).length) {
-                return;
-            }
+        if (trigger.is(to_element) || trigger.find(to_element).length) {
+            return;
         }
-        trigger.hide();
+        clearTimeout(trigger.data('yo-trigger-timeout'));
+        trigger.removeData('yo-trigger-timeout');
+        trigger.addClass('yo-notify-exit');
     });
-    */
 };
