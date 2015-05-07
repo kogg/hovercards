2edad49de10b71bbf494f2881c433979507e0463
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-05-06 18:22:14 -0700

    fixed weird wiggle and moved trigger styles into one file

--- app/scripts/embedded-trigger.js
+++ app/scripts/embedded-trigger.js
@@ -8,25 +8,26 @@ module.exports = function(body, selector, get_url, sendMessage, fullscreenable) 
     body = $(body);
     var trigger = body.find('.hovercards-embedded-trigger');
     if (!trigger.length) { // FIXME I don't like this at all
-        trigger = $('<div class="hovercards-embedded-trigger"></div>').appendTo(body);
-        trigger.hide();
-        trigger.mouseenter(function() {
-            trigger.show();
-        });
-        trigger.mouseleave(function(e) {
-            var to_element = e.toElement || e.relatedTarget;
-            if (to_element) { // TODO Unit Test this
-                to_element = $(to_element);
-                var obj = trigger.data('hovercards-obj'); // FIXME I REALLY DON'T LIKE THIS
-                if (obj.is(to_element) || obj.find(to_element).length) {
-                    return;
+        trigger = $('<div class="hovercards-embedded-trigger"></div>')
+            .appendTo(body)
+            .hide()
+            .mouseenter(function() {
+                trigger.show();
+            })
+            .mouseleave(function(e) {
+                var to_element = e.toElement || e.relatedTarget;
+                if (to_element) { // TODO Unit Test this
+                    to_element = $(to_element);
+                    var obj = trigger.data('hovercards-obj'); // FIXME I REALLY DON'T LIKE THIS
+                    if (obj.is(to_element) || obj.find(to_element).length) {
+                        return;
+                    }
                 }
-            }
-            trigger.hide();
-        });
-        trigger.click(function() {
-            sendMessage(trigger.data('hovercards-url'));
-        });
+                trigger.hide();
+            })
+            .click(function() {
+                sendMessage(trigger.data('hovercards-url'));
+            });
         longpress(body, 'div.hovercards-embedded-trigger', function() {
             return trigger.data('hovercards-url');
         });
@@ -45,15 +46,14 @@ module.exports = function(body, selector, get_url, sendMessage, fullscreenable) 
         if (!url) {
             return;
         }
-        trigger.show();
-        var obj_offset = obj.offset();
-        trigger.offset({ top: obj_offset.top + offset.top, left: obj_offset.left + offset.left });
-        trigger.data('hovercards-url', url);
-        trigger.data('hovercards-obj', obj); // FIXME I REALLY DON'T LIKE THIS
-        if (fullscreenable) {
-            trigger.addClass('hovercards-embedded-trigger-fullscreenable');
-        } else {
-            trigger.removeClass('hovercards-embedded-trigger-fullscreenable');
+        if (trigger.is(':hidden')) {
+            trigger.show();
+            var obj_offset = obj.offset();
+            trigger
+                .offset({ top: obj_offset.top + offset.top, left: obj_offset.left + offset.left })
+                .data('hovercards-url', url)
+                .data('hovercards-obj', obj) // FIXME I REALLY DON'T LIKE THIS
+                .toggleClass('hovercards-embedded-trigger-fullscreenable', fullscreenable);
         }
     });
     body.on('mouseleave', selector, function(e) {
