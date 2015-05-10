60f99d96aa5abc3c3ee23a54e38beb1c4942b38d
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-05-10 15:39:50 -0700

    .no-yo makes the yo's not show up

--- app/scripts/everywhere-main.js
+++ app/scripts/everywhere-main.js
@@ -8,37 +8,37 @@ var extension_id = chrome.i18n.getMessage('@@extension_id');
 
 var html = $('html');
 
-longpress(html, 'a[href]:not([href*="noyo=1"]):not([data-href])', function(link) {
+longpress(html, 'a[href]:not([data-href])', function(link) {
     return common.nullify_bad_url(common.relative_to_absolute(link.attr('href')));
 });
 
-longpress(html, 'a[data-href]:not([data-href*="noyo=1"])', function(link) {
+longpress(html, 'a[data-href]', function(link) {
     return common.nullify_bad_url(common.relative_to_absolute(link.data('href')));
 });
 
-yo_trigger(html, 'a[href]:not([href*="noyo=1"]):not([data-href])', function(link) {
+yo_trigger(html, 'a[href]:not([data-href]):not(.no-yo)', function(link) {
     return common.nullify_bad_url(common.relative_to_absolute(link.attr('href')));
 });
 
-yo_trigger(html, 'a[data-href]:not([data-href*="noyo=1"])', function(link) {
+yo_trigger(html, 'a[data-href]:not(.no-yo)', function(link) {
     return common.nullify_bad_url(common.relative_to_absolute(link.attr('href')));
 });
 
-embedded_trigger(html, 'embed[src]:not([src*="noyo=1"])', { top: 32, left: 8 }, function(embed) {
+embedded_trigger(html, 'embed[src]:not(.no-yo)', { top: 32, left: 8 }, function(embed) {
     return common.nullify_bad_url(common.relative_to_absolute(embed.attr('src')));
 });
 
-embedded_trigger(html, 'object[data]:not([data*="noyo=1"])', { top: 32, left: 8 }, function(object) {
+embedded_trigger(html, 'object[data]:not(.no-yo)', { top: 32, left: 8 }, function(object) {
     return common.nullify_bad_url(common.relative_to_absolute(object.attr('data')));
 });
 
 embedded_trigger(html, 'div#player div.html5-video-player', { top: 32, left: 8 }, function() {
-    return (document.URL.indexOf('noyo') === -1) && document.URL;
+    return document.URL;
 });
 
 if (window.top !== window) {
     embedded_trigger(html, 'body', { top: 8, left: 8 }, function() {
-        return (document.URL.indexOf('noyo') === -1) && (document.URL.indexOf('redditmedia.com') !== -1) && document.URL;
+        return (document.URL.indexOf('redditmedia.com') !== -1) && document.URL;
     });
 }
 
