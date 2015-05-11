251f5ab49817b9564142194570aae0cc91bf9788
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-05-10 18:16:17 -0700

    maybe we don't have an order yet

--- app/scripts/entry-components.js
+++ app/scripts/entry-components.js
@@ -71,6 +71,7 @@ module.exports = angular.module(chrome.i18n.getMessage('app_short_name') + 'Entr
             if ($scope.data.content.discussions && $scope.data.content.discussions.length) {
                 $scope.entry.discussions = ($scope.entry.discussions || []);
                 chrome.storage.sync.get('order', function(obj) {
+                    obj.order = obj.order || [];
                     $scope.$apply(function() {
                         ($scope.data.content.discussions || []).forEach(function(discussion) {
                             if (!$scope.entry.discussions.some(function(entry_discussion) { return discussion.api === entry_discussion.api; })) {
