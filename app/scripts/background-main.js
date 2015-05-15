557b5e323eb0101b42550a71651ed011804ce0a9
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-05-14 18:16:51 -0700

    reorganized the manifest.json, also made the user id be the chrome person or just a generated id

--- app/scripts/background-main.js
+++ app/scripts/background-main.js
@@ -1,44 +1,56 @@
-var $ = require('jquery');
+var $     = require('jquery');
+var async = require('async');
 
-chrome.storage.local.get('user_id', function(obj) {
-    if (!obj.user_id) {
-        obj.user_id = (Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2)).substr(0, 25);
-        chrome.storage.local.set(obj);
-    }
-
-    var client_side_calls = {
-        reddit: require('YoCardsAPICalls/reddit')({ key: 'fNtoQI4_wDq21w', user: obj.user_id })
-    };
-
-    chrome.runtime.onMessage.addListener(function(request, sender, sendMessage) {
-        request = $.extend({}, request);
-        var api  = request.api;
-        var type = request.type;
-        delete request.api;
-        delete request.type;
-
-        if (client_side_calls[api] && client_side_calls[api][type]) {
-            client_side_calls[api][type](request, function(err, result) {
-                err = err || (!result && { status: 404 });
-                if (err) {
-                    return sendMessage([err]);
-                }
-                sendMessage([err, result]);
-            });
-        } else {
-            $.ajax({ url:     'https://' + chrome.i18n.getMessage('app_short_name') + '.herokuapp.com/v1/' + api + '/' + type,
-                     data:    request,
-                     headers: { user: obj.user_id} })
-                .done(function(data) {
-                    sendMessage([null, data]);
-                })
-                .fail(function(err) {
-                    sendMessage([err]);
-                });
+var user_id;
+async.detectSeries([chrome.identity.getProfileUserInfo, chrome.storage.local.get],
+    function(fn, callback) {
+        fn(function(obj) {
+            if (chrome.runtime.lastError) {
+                console.error('error', chrome.runtime.lastError);
+                return callback(false);
+            }
+            user_id = obj.id;
+            callback(!!user_id);
+        });
+    },
+    function(got_one) {
+        if (!got_one) {
+            console.log('couldn\'t get an id');
         }
-        return true;
+        console.log(user_id);
+        var client_side_calls = {
+            reddit: require('YoCardsAPICalls/reddit')({ key: 'fNtoQI4_wDq21w', user: user_id })
+        };
+
+        chrome.runtime.onMessage.addListener(function(request, sender, sendMessage) {
+            request = $.extend({}, request);
+            var api  = request.api;
+            var type = request.type;
+            delete request.api;
+            delete request.type;
+
+            if (client_side_calls[api] && client_side_calls[api][type]) {
+                client_side_calls[api][type](request, function(err, result) {
+                    err = err || (!result && { status: 404 });
+                    if (err) {
+                        return sendMessage([err]);
+                    }
+                    sendMessage([err, result]);
+                });
+            } else {
+                $.ajax({ url:     'https://' + chrome.i18n.getMessage('app_short_name') + '.herokuapp.com/v1/' + api + '/' + type,
+                         data:    request,
+                         headers: { user: user_id } })
+                    .done(function(data) {
+                        sendMessage([null, data]);
+                    })
+                    .fail(function(err) {
+                        sendMessage([err]);
+                    });
+            }
+            return true;
+        });
     });
-});
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.id, { code: 'window.top.postMessage({ msg: \'activate\', url: \'' + tab.url + '\' }, \'*\');' });
