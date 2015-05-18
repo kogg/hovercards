91cde8a977a863007575554a3d22f4c18fbb8141
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-05-18 16:22:18 -0700

    overflow on html as well

--- app/scripts/sidebar-inject.js
+++ app/scripts/sidebar-inject.js
@@ -42,14 +42,17 @@ module.exports = function sidebarInjectOn(inject_into, body, dbl_clickable, send
         obj.toggleClass(extension_id + '-fullscreen', event.data.value || false);
     }, false);
 
-    var initial_overflow;
+    var initial_body_overflow;
+    var initial_html_overflow;
     var iframe = $('<iframe webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
         .appendTo(obj)
         .attr('src', chrome.extension.getURL('sidebar.html'))
         .attr('frameborder', '0')
         .mouseenter(function() {
-            initial_overflow = body.css('overflow-y');
+            initial_body_overflow = body.css('overflow-y');
             body.css('overflow-y', 'hidden');
+            initial_html_overflow = $('html').css('overflow-y');
+            $('html').css('overflow-y', 'hidden');
             if (common.get_scrollbar_width()) {
                 iframe
                     .css('overflow', 'auto')
@@ -59,7 +62,8 @@ module.exports = function sidebarInjectOn(inject_into, body, dbl_clickable, send
             }
         })
         .mouseleave(function() {
-            body.css('overflow-y', initial_overflow);
+            body.css('overflow-y', initial_body_overflow);
+            $('html').css('overflow-y', initial_html_overflow);
             if (common.get_scrollbar_width()) {
                 iframe
                     .css('overflow', 'hidden')
