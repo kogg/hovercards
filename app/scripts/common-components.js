76abb9c17d1ef4a1741a9ba461f2508ab5d8a8f3
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-05-20 20:49:08 -0700

    Weird transcluding magic to get everything to work

--- app/scripts/common-components.js
+++ app/scripts/common-components.js
@@ -1,6 +1,19 @@
 var angular = require('angular');
 
-module.exports = angular.module(chrome.i18n.getMessage('app_short_name') + 'CommonComponents', [require('angular-sanitize')])
+module.exports = angular.module(chrome.i18n.getMessage('app_short_name') + 'CommonComponents', [require('angular-sanitize'), require('angular-messages')])
+    .directive('err', [function() {
+        return {
+            restrict: 'A',
+            scope: {
+                err: '=',
+                api: '='
+            },
+            transclude: true,
+            templateUrl: function(element, attr) {
+                return 'templates/' + attr.type + '_exceptions.html';
+            }
+        };
+    }])
     .directive('readmore', ['$sanitize', function($sanitize) {
         require('dotdotdot');
 
