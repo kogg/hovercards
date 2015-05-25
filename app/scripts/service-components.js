2a3b1d4778dba74002b03f94cd06a20ab1fe9f1f
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-05-25 01:07:44 -0700

    Fix broken more_content section

--- app/scripts/service-components.js
+++ app/scripts/service-components.js
@@ -38,7 +38,8 @@ module.exports = angular.module(chrome.i18n.getMessage('app_short_name') + 'Serv
                         })
                         .then(function(obj) {
                             angular.extend(object, obj);
-                            if (params.type in { discussion: true, more_content: true } && (!object.comments || !object.comments.length)) {
+                            if ((params.type === 'discussion'   && (!object.comments || !object.comments.length)) ||
+                                (params.type === 'more_content' && (!object.content  || !object.content.length))) {
                                 object.$err = { 'empty-content': true };
                                 return $q.reject(object.$err);
                             }
