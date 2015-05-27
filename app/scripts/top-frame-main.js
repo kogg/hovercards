9fdb51993fde777cd490a0ad5568983f427a3097
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-05-26 21:46:36 -0700

    sidebar-trigger into top-frame-main

--- app/scripts/top-frame-main.js
+++ app/scripts/top-frame-main.js
@@ -9,10 +9,50 @@ var sidebar_obj = require('./sidebar-inject')(container, 'body', document, funct
 });
 
 var sidebar_frame;
-var sidebar_trigger = require('./sidebar-trigger')(function(msg) {
+var sidebar_trigger = (function sidebarTrigger(sendMessage) {
+    var ready;
+    var url;
+
+    return function(request) {
+        if (!request) {
+            return;
+        }
+        sendMessage = sendMessage || function() {};
+        switch (request.msg) {
+            case 'ready':
+                ready = true;
+                if (!url) {
+                    return;
+                }
+                sendMessage({ msg: 'load', url: url });
+                break;
+            case 'activate':
+                var msg;
+                if (request.url !== url) {
+                    msg = { msg: 'load', url: request.url };
+                    url = request.url;
+                } else {
+                    msg = { msg: 'hide' };
+                    url = null;
+                }
+                if (!ready) {
+                    return;
+                }
+                sendMessage(msg);
+                break;
+            case 'hide':
+                url = null;
+                if (!ready) {
+                    return;
+                }
+                sendMessage({ msg: 'hide' });
+                break;
+        }
+    };
+}(function(msg) {
     sidebar_frame.postMessage(msg, '*');
     sidebar_obj.trigger('sidebar.msg', [msg]);
-});
+}));
 
 window.addEventListener('message', function(event) {
     if (event && event.data && event.data.msg === 'ready') {
