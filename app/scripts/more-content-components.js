888b2b8b737db129f4ee1b0c10822d7a28acddb2
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-05-31 21:53:23 -0700

    moreContent won't reload something

--- app/scripts/more-content-components.js
+++ app/scripts/more-content-components.js
@@ -1,13 +1,16 @@
 var angular = require('angular');
 
 module.exports = angular.module(chrome.i18n.getMessage('app_short_name') + 'MoreContentComponents', [require('./service-components')])
-    .controller('MoreContentController', ['$scope', 'apiService', function($scope, apiService) {
-        $scope.$watch('entry.selectedPerson.selectedAccount', function(request) {
-            if (!request) {
-                $scope.data.moreContent = null;
+    .controller('MoreContentController', ['$scope', '$timeout', 'apiService', function($scope, $timeout, apiService) {
+        $scope.$watch('entry.selectedPerson.selectedAccount', function(account) {
+            $scope.data.moreContent = null;
+            if (!account) {
                 return;
             }
-            $scope.data.moreContent = apiService.get({ api: request.api, type: 'more_content', id: request.id });
+            account.moreContent = account.moreContent || apiService.get({ api: account.api, type: 'more_content', id: account.id });
+            $timeout(function() {
+                $scope.data.moreContent = account.moreContent;
+            }, 100);
         });
     }])
     .name;
