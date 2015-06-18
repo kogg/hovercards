ffdf244f86f37b5e7d7a80faf41f5bb41d980ecc
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-06-17 22:33:45 -0700

    better?

--- app/scripts/background-main.js
+++ app/scripts/background-main.js
@@ -2,6 +2,5 @@ require('./google-analytics-background')();
 require('./service-background')();
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(tab.id, { code: 'window.top.postMessage({ msg: \'activate\', url: \'' + tab.url + '\' }, \'*\');' });
-    chrome.runtime.sendMessage({ type: 'analytics', request: ['send', 'event', 'trigger', 'carlito', tab.url] });
+    chrome.tabs.executeScript(tab.id, { code: 'window.top.postMessage({ msg: \'activate\', by: \'carlito\', url: \'' + tab.url + '\' }, \'*\');' });
 });
