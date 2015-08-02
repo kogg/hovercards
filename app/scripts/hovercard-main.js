6837056a8d995ed37244bbba3ba89c17bfbe9a57
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-08-02 15:40:30 -0700

    Sending messages around

--- app/scripts/hovercard-main.js
+++ app/scripts/hovercard-main.js
@@ -1,1 +1,14 @@
-console.log('hey');
+var EXTENSION_ID = chrome.i18n.getMessage('@@extension_id');
+
+window.addEventListener('message', function(event) {
+    if (!event || !event.data) {
+        return;
+    }
+    switch (event.data.msg) {
+        case EXTENSION_ID + '-load':
+            console.log('GOT SOMETHING', event.data);
+            break;
+    }
+}, false);
+
+window.parent.postMessage({ msg: EXTENSION_ID + '-hovercard-ready' }, '*');
