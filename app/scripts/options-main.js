6c939de5d5658a2dd2aa0cf1415e14c58489d253
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-09-08 22:30:45 -0700

    javascript works

---
+++ app/scripts/options-main.js
@@ -0,0 +1,47 @@
+var $ = require('jquery');
+
+$(function() {
+    chrome.storage.sync.get('disabled', function(obj) {
+        var disabled;
+
+        function set_disabled(new_disabled) {
+            disabled = new_disabled;
+            if (!disabled) {
+                return;
+            }
+            $('#imgur-content').prop('checked',      disabled.imgur      && disabled.imgur.content);
+            $('#imgur-account').prop('checked',      disabled.imgur      && disabled.imgur.account);
+            $('#instagram-content').prop('checked',  disabled.instagram  && disabled.instagram.content);
+            $('#instagram-account').prop('checked',  disabled.instagram  && disabled.instagram.account);
+            $('#reddit-content').prop('checked',     disabled.reddit     && disabled.reddit.content);
+            $('#reddit-account').prop('checked',     disabled.reddit     && disabled.reddit.account);
+            $('#soundcloud-content').prop('checked', disabled.soundcloud && disabled.soundcloud.content);
+            $('#soundcloud-account').prop('checked', disabled.soundcloud && disabled.soundcloud.account);
+            $('#twitter-content').prop('checked',    disabled.twitter    && disabled.twitter.content);
+            $('#twitter-account').prop('checked',    disabled.twitter    && disabled.twitter.account);
+            $('#youtube-content').prop('checked',    disabled.youtube    && disabled.youtube.content);
+            $('#youtube-account').prop('checked',    disabled.youtube    && disabled.youtube.account);
+        }
+        set_disabled(obj.disabled);
+
+        chrome.storage.onChanged.addListener(function(changes, area_name) {
+            if (area_name !== 'sync' || !('disabled' in changes)) {
+                return;
+            }
+            set_disabled(changes.disabled.newValue);
+        });
+
+        $('#save').on('click', function() {
+            console.log('trying to save...');
+            chrome.storage.sync.set({ disabled : { imgur:      { content: $('#imgur-content').prop('checked'),      account: $('#imgur-account').prop('checked') },
+                                                   instagram:  { content: $('#instagram-content').prop('checked'),  account: $('#instagram-account').prop('checked') },
+                                                   reddit:     { content: $('#reddit-content').prop('checked'),     account: $('#reddit-account').prop('checked') },
+                                                   soundcloud: { content: $('#soundcloud-content').prop('checked'), account: $('#soundcloud-account').prop('checked') },
+                                                   twitter:    { content: $('#twitter-content').prop('checked'),    account: $('#twitter-account').prop('checked') },
+                                                   youtube:    { content: $('#youtube-content').prop('checked'),    account: $('#youtube-account').prop('checked') } } },
+                function() {
+                    console.log('saved!');
+                });
+        });
+    });
+});
