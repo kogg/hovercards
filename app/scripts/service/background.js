474f26ad8831d648c143939ae345021430a30186
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-10-04 13:01:39 -0700

    smart tab service files

--- app/scripts/service/background.js
+++ app/scripts/service/background.js
@@ -1,17 +1,17 @@
 chrome.runtime.onMessage.addListener(function(message, sender, callback) {
-    if (message.type !== 'service') {
-        return;
-    }
+	if (message.type !== 'service') {
+		return;
+	}
 
-    var old_callback = callback;
-    callback = function(err, result) {
-        old_callback([err, result]);
-    };
+	var old_callback = callback;
+	callback = function(err, result) {
+		old_callback([err, result]);
+	};
 
-    // FIXME Mock BS
-    setTimeout(function() {
-        callback(null, { some: 'thing' });
-    }, 500 + 500 * Math.random());
+	// FIXME Mock BS
+	setTimeout(function() {
+		callback(null, { some: 'thing' });
+	}, 500 + 500 * Math.random());
 
-    return true;
+	return true;
 });
