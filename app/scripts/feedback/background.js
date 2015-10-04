2f4fe661c961d943ed120a6ebc4839d456548562
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-10-04 12:56:20 -0700

    smart tab feedback files

--- app/scripts/feedback/background.js
+++ app/scripts/feedback/background.js
@@ -2,18 +2,18 @@ var $   = require('jquery');
 var env = require('env');
 
 chrome.storage.sync.get(['feedback_url', 'last_feedback_retrieval'], function(obj) {
-    (function retrieve_feedback_url() {
-        setTimeout(function() {
-            $.ajax({ url: env.endpoint + '/feedback_url' })
-                .done(function(data) {
-                    obj.feedback_url = data.feedback_url;
-                    chrome.storage.sync.set({ feedback_url: obj.feedback_url });
-                })
-                .always(function() {
-                    obj.last_feedback_retrieval = Date.now();
-                    chrome.storage.sync.set({ last_feedback_retrieval: obj.last_feedback_retrieval });
-                    retrieve_feedback_url();
-                });
-        }, Math.max(0, (obj.last_feedback_retrieval || 0) + 24 * 60 * 60 * 1000 - Date.now()));
-    }());
+	(function retrieve_feedback_url() {
+		setTimeout(function() {
+			$.ajax({ url: env.endpoint + '/feedback_url' })
+				.done(function(data) {
+					obj.feedback_url = data.feedback_url;
+					chrome.storage.sync.set({ feedback_url: obj.feedback_url });
+				})
+				.always(function() {
+					obj.last_feedback_retrieval = Date.now();
+					chrome.storage.sync.set({ last_feedback_retrieval: obj.last_feedback_retrieval });
+					retrieve_feedback_url();
+				});
+		}, Math.max(0, (obj.last_feedback_retrieval || 0) + 24 * 60 * 60 * 1000 - Date.now()));
+	}());
 });
