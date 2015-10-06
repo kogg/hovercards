bddb79d07cf006f744840fa7e064e8631b65981f
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-10-05 17:57:56 -0700

    everything setup except for calling backend

--- app/scripts/service/background.js
+++ app/scripts/service/background.js
@@ -1,37 +1,91 @@
 var _ = require('underscore');
 
-var APIS = { imgur:      true,
-             instagram:  true,
-             reddit:     true,
-             soundcloud: true,
-             twitter:    true,
-             youtube:    true };
+var ALPHANUMERIC   = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+var REDDIT_KEY     = '0jXqEudQPqSL6w';
+var SOUNDCLOUD_KEY = '78a827254bd7a5e3bba61aa18922bf2e';
 
-var api_callers = _.mapObject(APIS, function(/* val, api */) {
-	return {
-		content: function(args, callback) {
-			// FIXME Mock BS
-			setTimeout(function() {
-				callback(null, { some: 'thing' });
-			}, 500 + 500 * Math.random());
-		},
-		account: function(args, callback) {
-			// FIXME Mock BS
-			setTimeout(function() {
-				callback(null, { some: 'thing' });
-			}, 500 + 500 * Math.random());
-		}
-	};
-});
+var device_id;
 
-chrome.runtime.onMessage.addListener(function(message, sender, callback) {
-	if (!message || message.type !== 'service' || !message.identity || !api_callers[message.identity.api] || !api_callers[message.identity.api][message.identity.type]) {
-		return;
+function initialize_caller(api, opts) {
+	opts = opts || {};
+	var caller = {};
+
+	function setup_server_caller() {
+		_.each(['content', 'account'], function(type) {
+			caller[type] = function(args, callback) {
+				chrome.storage.sync.get(api + '_user', function() {
+					if (chrome.runtime.lastError) {
+						return callback(chrome.runtime.lastError);
+					}
+					// TODO Call the backend server
+					setTimeout(function() {
+						callback(null, { some: 'thing' });
+					}, 500 + 500 * Math.random());
+				});
+			};
+		});
 	}
 
-	api_callers[message.identity.api][message.identity.type](message.identity, _.wrap(callback, function(callback, err, result) {
-		callback([err, result]);
-	}));
+	if (opts.client) {
+		chrome.storage.sync.get(api + '_user', function(obj) {
+			if (chrome.runtime.lastError) {
+				// TODO
+				return;
+			}
+			var user_id = obj.user_id;
+			function setup_client_caller() {
+				if (opts.client_on_auth && (!user_id || !user_id.length)) {
+					return setup_server_caller();
+				}
+				var client = opts.client(_.extend({ device: device_id, user: user_id }, opts.client_args));
+				caller.content = client.content;
+				caller.account = client.account;
+			}
 
-	return true;
+			setup_client_caller();
+			chrome.storage.onChanged.addListener(function(changes, area_name) {
+				if (area_name !== 'sync' || !((api + '_user') in changes)) {
+					return;
+				}
+				user_id = changes[api + '_user'].newValue;
+				setup_client_caller();
+			});
+		});
+	} else {
+		setup_server_caller();
+	}
+
+	return caller;
+}
+
+chrome.storage.local.get('device_id', function(obj) {
+	if (chrome.runtime.lastError) {
+		// TODO
+		return;
+	}
+	if (chrome.runtime.lastError || !obj || !obj.device_id || !obj.device_id.length) {
+		device_id = _.times(25, _.partial(_.sample, ALPHANUMERIC, null)).join('');
+		chrome.storage.local.set({ device_id: device_id });
+	} else {
+		device_id = obj.device_id;
+	}
+
+	var api_callers = { imgur:      initialize_caller('imgur'),
+						instagram:  initialize_caller('instagram',  { client: require('hovercardsshared/old-apis/instagram'),  client_on_auth: true }),
+						reddit:     initialize_caller('reddit',     { client: require('hovercardsshared/old-apis/reddit'),     client_args: { key: REDDIT_KEY } }),
+						soundcloud: initialize_caller('soundcloud', { client: require('hovercardsshared/old-apis/soundcloud'), client_args: { key: SOUNDCLOUD_KEY } }),
+						twitter:    initialize_caller('twitter'),
+						youtube:    initialize_caller('youtube') };
+
+	chrome.runtime.onMessage.addListener(function(message, sender, callback) {
+		if (!message || message.type !== 'service' || !message.identity || !api_callers[message.identity.api] || !api_callers[message.identity.api][message.identity.type]) {
+			return;
+		}
+
+		api_callers[message.identity.api][message.identity.type](message.identity, _.wrap(callback, function(callback, err, result) {
+			callback([err, result]);
+		}));
+
+		return true;
+	});
 });
