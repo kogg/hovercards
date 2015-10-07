2c34001b8d0c4767e984fc888ac4c7432f0645de
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-10-07 16:25:13 -0700

    more consistent way of writing this

--- app/scripts/analytics/top-frame.js
+++ app/scripts/analytics/top-frame.js
@@ -5,6 +5,24 @@ var env = require('env');
 var ALPHANUMERIC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 var EXTENSION_ID = chrome.i18n.getMessage('@@extension_id');
 
+window.addEventListener('message', function(event) {
+	if (!event || !event.data) {
+		return;
+	}
+	var message = event.data;
+	if (message.msg !== EXTENSION_ID + '-analytics') {
+		return;
+	}
+	$.analytics.apply(this, message.request);
+});
+
+if (!env.analytics_id) {
+	$.analytics = function() {
+		console.debug('google analytics', Array.prototype.slice.call(arguments));
+	};
+	return;
+}
+
 function get_user_id(callback) {
 	chrome.storage.sync.get('user_id', function(obj) {
 		if (chrome.runtime.lastError || !obj || !obj.user_id) {
@@ -18,50 +36,33 @@ function get_user_id(callback) {
 	});
 }
 
-if (env.analytics_id) {
-	var analytics_queue = [];
+var analytics_queue = [];
+$.analytics = function() {
+	analytics_queue.push(arguments);
+};
+get_user_id(function(err, user_id) {
+	if (err) {
+		return console.error('error getting user_id', err);
+	}
+
 	$.analytics = function() {
-		analytics_queue.push(arguments);
-	};
-	get_user_id(function(err, user_id) {
-		if (err) {
-			return console.error('error getting user_id', err);
-		}
-
-		$.analytics = function() {
-			window.GoogleAnalyticsObject = 'ga';
-			window.ga = window.ga || function() {
-				(window.ga.q = window.ga.q || []).push(arguments);
-			};
-			window.ga.l = 1 * new Date();
-			require('./analytics-local');
-
-			window.ga('create', env.analytics_id, { 'userId': user_id });
-			window.ga('set', { appName: chrome.i18n.getMessage('app_name'), appVersion: chrome.runtime.getManifest().version });
-			window.ga('send', 'screenview', { screenName: 'None' });
-			window.ga.apply(this, Array.prototype.slice.call(arguments));
-			$.analytics = function() {
-				window.ga.apply(this, Array.prototype.slice.call(arguments));
-			};
+		window.GoogleAnalyticsObject = 'ga';
+		window.ga = window.ga || function() {
+			(window.ga.q = window.ga.q || []).push(arguments);
 		};
+		window.ga.l = 1 * new Date();
+		require('./analytics-local');
 
-		analytics_queue.forEach(function(args) {
-			$.analytics.apply($.analytics, args);
-		});
-	});
-} else {
-	$.analytics = function() {
-		console.debug('google analytics', Array.prototype.slice.call(arguments));
+		window.ga('create', env.analytics_id, { 'userId': user_id });
+		window.ga('set', { appName: chrome.i18n.getMessage('app_name'), appVersion: chrome.runtime.getManifest().version });
+		window.ga('send', 'screenview', { screenName: 'None' });
+		window.ga.apply(this, Array.prototype.slice.call(arguments));
+		$.analytics = function() {
+			window.ga.apply(this, Array.prototype.slice.call(arguments));
+		};
 	};
-}
 
-window.addEventListener('message', function(event) {
-	if (!event || !event.data) {
-		return;
-	}
-	var message = event.data;
-	if (message.msg !== EXTENSION_ID + '-analytics') {
-		return;
-	}
-	$.analytics.apply(this, message.request);
+	analytics_queue.forEach(function(args) {
+		$.analytics.apply($.analytics, args);
+	});
 });
