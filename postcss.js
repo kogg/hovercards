3a5e8c619948dc71559b757013b20e800a3ec4a8
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-10-14 16:24:31 -0700

    postcss config as a javascript file

--- postcss.js
+++ postcss.js
@@ -1,4 +1,10 @@
-{
+module.exports = {
 	"use": ["postcss-import", "postcss-class-prefix", "postcss-color-rgba-fallback", "autoprefixer", "postcss-reporter"],
+	"postcss-import": {
+		onImport: function(sources) {
+			console.log('postcss', sources);
+			global.watchCSS(sources);
+		}
+	},
 	"postcss-class-prefix": "__MSG_@@extension_id__-"
-}
+};
