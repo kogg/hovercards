51ba31d5bbdd1b5eed5271018ce76f9114bddfe5
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-10-20 18:50:12 -0700

    use templates from hovercardsshared

--- app/template_loading/index.js
+++ app/template_loading/index.js
@@ -4,10 +4,33 @@ var service = require('../service');
 
 Ractive.DEBUG = process.env.NODE_ENV !== 'production';
 
+var templates = {};
+
 module.exports = function(obj, identity) {
+	var type = _.result(identity, 'type');
+
+	if (!templates[type]) {
+		switch (type) {
+			case 'content':
+				templates.content = require('hovercardsshared/*/content.html', { mode: 'hash' });
+				break;
+			case 'discussion':
+				templates.discussion = require('hovercardsshared/*/discussion.html', { mode: 'hash' });
+				break;
+			case 'account':
+				templates.account = require('hovercardsshared/*/account.html', { mode: 'hash' });
+				break;
+			case 'account_content':
+				templates.account_content = require('hovercardsshared/*/account_content.html', { mode: 'hash' });
+				break;
+			default:
+				return;
+		}
+	}
+
 	return new Ractive({
 		el: obj,
-		template: '{{#each .}}{{ @key }} {{ . }}<br>{{/each}}',
+		template: templates[type][_.result(identity, 'api')] || 'There is no template for this',
 		data: function() {
 			var instance = this;
 
