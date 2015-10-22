0bba115597d2f912513c720f55219d417f185a0b
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-10-22 12:29:12 -0700

    the account template needs content to have something for the "if" statement to register that content is there, just not loaded

--- app/template_loading/index.js
+++ app/template_loading/index.js
@@ -63,7 +63,7 @@ module.exports = function(obj, identity, expanded) {
 					});
 					break;
 				case 'account':
-					ractive.set('content', { });
+					ractive.set('content', { loaded: false });
 					service(_.defaults({ type: 'account_content' }, identity), function(err, data) {
 						if (err) {
 							return ractive.set('content', { loaded: true, err: err });
