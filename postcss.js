7fd4bbd011ff3e32a144361bf8f63eb57cfc6e15
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-10-27 17:04:26 -0700

    new version of postcss-cli deals with postcss-import weirdness

--- postcss.js
+++ postcss.js
@@ -1,5 +1,3 @@
-var source_index = {};
-
 module.exports = {
 	use: [
 		'postcss-import',
@@ -12,15 +10,7 @@ module.exports = {
 	],
 	'postcss-class-prefix': '__MSG_@@extension_id__-',
 	'postcss-import': {
-		glob: true,
-		onImport: function(sources) {
-			source_index[this.from || sources[0]] = sources;
-			var files = Object.keys(source_index).reduce(function(memo, from) {
-				return memo.concat(source_index[from]);
-			}, []);
-			console.log('postcss', files);
-			global.watchCSS(files);
-		}
+		glob: true
 	},
 	'postcss-map-url': function(path) {
 		return (path.indexOf('data:') === 0 || path.indexOf('http') === 0) ? path : 'chrome-extension://__MSG_@@extension_id__/' + path;
