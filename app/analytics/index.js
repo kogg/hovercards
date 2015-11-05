fe6814cd31acbd4c0401e6b668d715f5feb98ff7
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-11-04 20:54:52 -0800

    better

--- app/analytics/index.js
+++ app/analytics/index.js
@@ -3,7 +3,7 @@ var _ = require('underscore');
 module.exports = function() {
 	var args = _.toArray(arguments);
 	chrome.runtime.sendMessage({ type: 'analytics', request: args }, (process.env.NODE_ENV !== 'production') && function() {
-		if (args[0] === 'send' && args[1] === 'exception') {
+		if (_.chain(args).first(2).isEqual(['send', 'exception']).value()) {
 			console.error('google analytics', args);
 		} else {
 			console.debug('google analytics', args);
