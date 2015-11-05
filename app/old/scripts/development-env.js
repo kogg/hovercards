31c86c0974d324b1c0f966833235528108a44811
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-11-04 20:33:16 -0800

    delete old code

--- app/old/scripts/development-env.js
+++
@@ -1,2 +0,0 @@
-// module.exports.endpoint    = 'http://hover.cards/v2';
-module.exports.endpoint = 'http://localhost:5000/v2';
--- app/old/scripts/production-env.js
+++
@@ -1,2 +0,0 @@
-module.exports.analytics_id = 'UA-64246820-3';
-module.exports.endpoint     = 'http://hover.cards/v2';
