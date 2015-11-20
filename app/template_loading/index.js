e7f8940720e981b58677cb983132b68292d1b5ea
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-11-20 00:21:57 -0800

    Revert moving number decorator to instead be a function

--- app/template_loading/index.js
+++ app/template_loading/index.js
@@ -14,25 +14,6 @@ var global_data = {
 		name = name.replace(/\-/g, '_');
 		return (!_.isEmpty(api) && chrome.i18n.getMessage(api + '_' + name, rest)) || chrome.i18n.getMessage(name, rest);
 	},
-	number: function(number) {
-		if (!_.isNumber(number) || _.isNaN(number)) {
-			return number;
-		}
-		if (number < 10000) {
-			return number.toLocaleString();
-		}
-
-		var log1000      = Math.floor(Math.log10(number) / 3);
-		var multiple1000 = number / Math.pow(1000, log1000);
-		var roundto      = Math.pow(10, 2 - Math.floor(Math.log10(multiple1000)));
-		multiple1000     = Math.round(multiple1000 * roundto) / roundto;
-
-		if (!log1000) {
-			// Shouldn't happen
-			return multiple1000.toLocaleString();
-		}
-		return global_data.copy(['', 'x-thousand', 'x-million', 'x-billion', 'x-trillion'][log1000], null, multiple1000.toLocaleString());
-	},
 	prefix: _.prefix,
 	url: urls.print
 };
