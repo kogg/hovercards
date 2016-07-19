dabc4d14c03f00150b3ed9488269fa739936f80b
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-07-19 12:29:32 -0700

    Fixing bug @marcomarandiz experienced
    It was ignoring the reddit comments link magic if the disabled for
    reddit had never been changed before, which was silly. Now it only
    ignores them if it's been defined, and is set to be disabled

--- app/hovercard/every-frame.js
+++ app/hovercard/every-frame.js
@@ -251,7 +251,7 @@ HOVERABLE_THINGS.forEach(function(hoverable) {
 			return;
 		}
 		if (!(url = massage_url(hoverable.get_url(obj))) || !(identity = urls.parse(url)) || !accept_identity(identity, obj)) {
-			if (!disabled || !disabled.reddit || disabled.reddit.content) {
+			if (disabled && disabled.reddit && disabled.reddit.content) {
 				return;
 			}
 			if (!document.location.hostname.endsWith('reddit.com') || !obj.hasClass('title')) {
