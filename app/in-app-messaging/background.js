c10aea68448246454646d0093b85da0c2bb7f0e4
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-07-20 13:02:01 -0700

    Accept messages from ractive template
    Sends analytics and remembers never to open the message again.
    
    Closes #47

--- app/in-app-messaging/background.js
+++ app/in-app-messaging/background.js
@@ -1,9 +1,11 @@
-var $      = require('jquery');
-var _      = require('underscore');
-var async  = require('async');
-var config = require('../config');
+var $     = require('jquery');
+var _     = require('underscore');
+var async = require('async');
 
-var WHENTOSTART  = 3;
+var analytics = require('../analytics/background');
+var config    = require('../config');
+
+var WHENTOSTART  = 1;
 var numberOfGets = 0;
 
 $.ajax({
@@ -12,7 +14,8 @@ $.ajax({
 	jsonp:    false
 })
 	.done(function(messages) {
-		chrome.storage.sync.get('messages_done', function(messages_done) {
+		chrome.storage.sync.get('messages_done', function(obj) {
+			var messages_done = (obj || {}).messages_done || [];
 			if (!messages_done) {
 				return;
 			}
@@ -53,6 +56,38 @@ $.ajax({
 							return callback();
 						}
 						callback(_.defaults({ count: message_count }, inAppMessage));
+					});
+					return true;
+				case 'ignored':
+				case 'visited':
+					async.waterfall([
+						function(callback) {
+							async.parallel({
+								messages_done: function(callback) {
+									chrome.storage.sync.get('messages_done', function(obj) {
+										callback(chrome.runtime.lastError, (obj || {}).messages_done || []);
+									});
+								},
+								messages_counts: function(callback) {
+									chrome.storage.sync.get('messages_counts', function(obj) {
+										callback(chrome.runtime.lastError, (obj || {}).messages_counts || []);
+									});
+								}
+							}, callback);
+						},
+						function(results, callback) {
+							analytics('send', 'event', 'message ' + message.action, 'link clicked', message.id, results.messages_counts[message.id] || 1);
+							results.messages_done.push(message.id);
+							results.messages_done = _.uniq(results.messages_done);
+							messages = _.reject(messages, function(message) {
+								return _.contains(results.messages_done, message.id);
+							});
+							chrome.storage.sync.set({ messages_done: results.messages_done }, function(thing) {
+								callback(chrome.runtime.lastError);
+							});
+						}
+					], function(err) {
+						callback(err);
 					});
 					return true;
 			}
