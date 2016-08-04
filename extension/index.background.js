e12650392f702b0d4280be557055f955331c6b67
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-08-03 19:06:47 -0700

    Set Uninstall URL
     #55

--- extension/index.background.js
+++ extension/index.background.js
@@ -1,5 +1,6 @@
 var createStore = require('../redux/createStore');
 
 require('./content-security-policy');
+require('./set-uninstall-url');
 
 createStore();
---
+++ extension/set-uninstall-url.js
@@ -0,0 +1,3 @@
+var browser = require('./browser');
+
+browser.runtime.setUninstallURL('https://hovercards.typeform.com/to/ajyJv2');
