34b9bb1ee231544c70f94c7344de4148356cd6e3
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-09-11 11:57:01 -0700

    feat(sentry): Associate errors with the users
    We can have a better picture of whether errors are widespread or very specific

--- report/index.browser.js
+++ report/index.browser.js
@@ -1,4 +1,7 @@
+var _     = require('underscore');
 var Raven = require('raven-js');
+
+var browser = require('../extension/browser');
 
 Raven
 	.config(process.env.SENTRY_DSN_CLIENT, {
@@ -6,5 +9,28 @@ Raven
 		release:     process.env.npm_package_version
 	})
 	.install();
+
+browser.storage.onChanged.addListener(function(changes, areaName) {
+	if (areaName !== 'sync') {
+		return;
+	}
+	_.pairs(changes).forEach(function(entry) {
+		if (entry[0] !== 'user_id') {
+			return;
+		}
+		Raven.setUserContext({ id: entry[1].newValue });
+	});
+});
+
+browser.storage.sync.get('user_id')
+	.then(_.property('user_id'))
+	.then(function(user_id) {
+		if (!user_id) {
+			return;
+		}
+
+		Raven.setUserContext({ id: user_id });
+	})
+	.catch(Raven.captureException);
 
 module.exports = Raven;
